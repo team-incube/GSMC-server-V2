781175b7a11a477588bdac51ebdc33dcb8618f9d
KIM TAEEUN <snowykte0426@naver.com>
2025-04-04 20:49:09 +0900

    add: 임시 보안설정 추가

---
+++ src/main/java/team/incude/gsmc/v2/global/security/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package team.incude.gsmc.v2.global.security.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+/*
+TODO: 인증/인가 구현 또는 보안 관련 설정 시 해당 클래스를 무조건 변경하여주세요.
+ */
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll()
+                )
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable);
+
+        return http.build();
+    }
+}
