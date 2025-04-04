c73218a11d18b6044c37cee256b33a7ad65d058e
KIM TAEEUN <snowykte0426@naver.com>
2025-04-04 09:36:36 +0900

    chore: 미사용 의존성 제거

--- src/main/java/team/incude/gsmc/v2/domain/auth/persistence/entity/AuthenticationRedisEntity.java
+++ src/main/java/team/incude/gsmc/v2/domain/auth/persistence/entity/AuthenticationRedisEntity.java
@@ -1,13 +1,11 @@
 package team.incude.gsmc.v2.domain.auth.persistence.entity;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
 import org.springframework.data.redis.core.TimeToLive;
-import org.springframework.data.redis.core.index.Indexed;
 
 import java.util.concurrent.TimeUnit;
 
