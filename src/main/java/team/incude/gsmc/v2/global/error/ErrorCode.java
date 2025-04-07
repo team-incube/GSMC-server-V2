578ed8ec3920510f2f89cbbe0fdb379930894fe9
KIM TAEEUN <snowykte0426@naver.com>
2025-04-07 23:53:36 +0900

    add: 항목/자격증/사용자 미존재,점수 제한 초과 등의 예외 클래스 선언

--- src/main/java/team/incude/gsmc/v2/global/error/ErrorCode.java
+++ src/main/java/team/incude/gsmc/v2/global/error/ErrorCode.java
@@ -2,14 +2,27 @@ package team.incude.gsmc.v2.global.error;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
 
     // AWS S3
-    S3_UPLOAD_FAILED("AWS S3 Upload Failed", 500),
-    S3_DELETE_FAILED("AWS S3 Delete Failed", 500);
+    S3_UPLOAD_FAILED("AWS S3 Upload Failed", HttpStatus.INTERNAL_SERVER_ERROR.value()),
+    S3_DELETE_FAILED("AWS S3 Delete Failed", HttpStatus.INTERNAL_SERVER_ERROR.value()),
+
+    // Certificate
+    CERTIFICATE_NOT_FOUND("Certificate Not Found", HttpStatus.NOT_FOUND.value()),
+
+    // Score
+    SCORE_MAXIMUM_VALUE_EXCEEDED("Score Maximum Value Exceeded", HttpStatus.UNPROCESSABLE_ENTITY.value()),
+
+    // Member
+    MEMBER_NOT_FOUND("Member Not Found", HttpStatus.NOT_FOUND.value()),
+
+    // Category
+    CATEGORY_NOT_FOUND("Category Not Found", HttpStatus.NOT_FOUND.value());
 
     private final String message;
     private final int status;
