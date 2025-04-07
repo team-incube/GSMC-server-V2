1279ef5d129eec3fa98aa69698a6eb1d6240cb13
KIM TAEEUN <snowykte0426@naver.com>
2025-04-07 13:57:47 +0900

    add: 전역 예외 처리 패키지 추가

---
+++ src/main/java/team/incude/gsmc/v2/global/error/ErrorCode.java
@@ -0,0 +1,15 @@
+package team.incude.gsmc.v2.global.error;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+
+    // AWS S3
+    S3_UPLOAD_FAILED("AWS S3 Upload Failed", 500);
+
+    private final String message;
+    private final int status;
+}
