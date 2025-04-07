754a3dda5a63ce4f36ae290219a2aa295f35a8ba
KIM TAEEUN <snowykte0426@naver.com>
2025-04-07 23:54:09 +0900

    add: 증빙자료 저장 메서드 추가 구현

--- src/main/java/team/incude/gsmc/v2/domain/evidence/application/port/EvidencePersistencePort.java
+++ src/main/java/team/incude/gsmc/v2/domain/evidence/application/port/EvidencePersistencePort.java
@@ -1,11 +1,10 @@
 package team.incude.gsmc.v2.domain.evidence.application.port;
 
-import team.incude.gsmc.v2.domain.evidence.domain.OtherEvidence;
+import team.incude.gsmc.v2.domain.evidence.domain.Evidence;
 import team.incude.gsmc.v2.global.annotation.PortDirection;
 import team.incude.gsmc.v2.global.annotation.port.Port;
 
-import java.util.List;
-
 @Port(direction = PortDirection.OUTBOUND)
 public interface EvidencePersistencePort {
+    void saveEvidence(Evidence evidence);
 }
