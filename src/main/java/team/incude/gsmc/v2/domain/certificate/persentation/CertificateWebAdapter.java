8abc4888811fa2efd86f4114aeb6deae20721ba6
KIM TAEEUN <snowykte0426@naver.com>
2025-04-07 11:24:31 +0900

    add: 현재 인증된 사용자 자격증 조회 및 자격증 등록 엔드포인트 정의

--- src/main/java/team/incude/gsmc/v2/domain/certificate/persentation/CertificateWebAdapter.java
+++ src/main/java/team/incude/gsmc/v2/domain/certificate/persentation/CertificateWebAdapter.java
@@ -3,12 +3,12 @@ package team.incude.gsmc.v2.domain.certificate.persentation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import team.incude.gsmc.v2.domain.certificate.application.port.CertificateApplicationPort;
 import team.incude.gsmc.v2.domain.certificate.persentation.data.response.GetCertificatesResponse;
+
+import java.time.LocalDate;
 
 @RestController
 @RequestMapping("/api/v2/certificates")
@@ -17,8 +17,23 @@ public class CertificateWebAdapter {
 
     private final CertificateApplicationPort certificateApplicationPort;
 
+    @GetMapping("/current")
+    public ResponseEntity<GetCertificatesResponse> getCurrentCertificates() {
+        return ResponseEntity.status(HttpStatus.OK).body(certificateApplicationPort.findCurrentCertificate());
+    }
+
     @GetMapping("/{email}")
     public ResponseEntity<GetCertificatesResponse> getCertificates(@PathVariable String email) {
         return ResponseEntity.status(HttpStatus.OK).body(certificateApplicationPort.findCertificateByEmail(email));
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> createCertificates(
+            @RequestParam("name") String name,
+            @RequestParam("acquisitionDate") LocalDate acquisitionDate,
+            @RequestParam("file") MultipartFile file
+    ) {
+        certificateApplicationPort.createCertificate(name, acquisitionDate, file);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 }
