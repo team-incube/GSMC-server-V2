0bf5e8b6502f5cca952c28b7122787ae2c2d93b6
jihoonwjj <wangjajihooon@gmail.com>
2025-04-08 18:05:24 +0900

    add: getCurrentUser 추가

---
+++ src/main/java/team/incude/gsmc/v2/global/security/CurrentUserProvider.java
@@ -0,0 +1,30 @@
+package team.incude.gsmc.v2.global.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import team.incude.gsmc.v2.domain.member.exception.MemberNotFoundException;
+import team.incude.gsmc.v2.domain.member.persistence.entity.MemberJpaEntity;
+import team.incude.gsmc.v2.domain.member.persistence.repository.MemberJpaRepository;
+import team.incude.gsmc.v2.global.security.exception.MemberUnauthorizedException;
+import team.incude.gsmc.v2.global.security.jwt.auth.CustomUserDetails;
+
+@Component
+@RequiredArgsConstructor
+public class CurrentUserProvider {
+
+    private final MemberJpaRepository memberJpaRepository;
+
+    public MemberJpaEntity getCurrentUser() {
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        if (principal instanceof CustomUserDetails) {
+            String userId = ((CustomUserDetails) principal).getUsername();
+            return memberJpaRepository.findById(Long.valueOf(userId))
+                    .orElseThrow(() -> new MemberNotFoundException());
+        } else {
+            throw new MemberUnauthorizedException();
+        }
+    }
+}
