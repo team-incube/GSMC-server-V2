30ce20a437d29cf7dd4b87cbeccfbdfe5f3b35f8
KIM TAEEUN <snowykte0426@naver.com>
2025-04-08 18:07:34 +0900

    update: 누락된 칼럼명 설정 추가

--- src/main/java/team/incude/gsmc/v2/domain/evidence/persistence/entity/ReadingEvidenceJpaEntity.java
+++ src/main/java/team/incude/gsmc/v2/domain/evidence/persistence/entity/ReadingEvidenceJpaEntity.java
@@ -12,6 +12,7 @@ import lombok.NoArgsConstructor;
 public class ReadingEvidenceJpaEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "evidence_id")
     private Long id;
 
     @OneToOne
