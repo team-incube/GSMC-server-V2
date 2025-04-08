bb1695232bcef789070a3a80bf3d86516e792711
suuuuuuminnnnnn <s24035@gsm.hs.kr>
2025-04-08 17:18:44 +0900

    add: EvidencePersistence Port/Adapter 구현

--- src/main/java/team/incude/gsmc/v2/domain/evidence/application/port/EvidencePersistencePort.java
+++ src/main/java/team/incude/gsmc/v2/domain/evidence/application/port/EvidencePersistencePort.java
@@ -4,8 +4,14 @@ import team.incude.gsmc.v2.domain.evidence.domain.Evidence;
 import team.incude.gsmc.v2.global.annotation.PortDirection;
 import team.incude.gsmc.v2.global.annotation.port.Port;
 
+import java.util.List;
+
 @Port(direction = PortDirection.OUTBOUND)
 public interface EvidencePersistencePort {
+    List<Evidence> findEvidencesByEmail(String email);
+
+    void deleteEvidenceByEvidenceId(Long evidenceId);
+
     Evidence findEvidenceById(Long id);
 
     Evidence saveEvidence(Evidence evidence);
--- src/main/java/team/incude/gsmc/v2/domain/evidence/persistence/EvidencePersistenceAdapter.java
+++ src/main/java/team/incude/gsmc/v2/domain/evidence/persistence/EvidencePersistenceAdapter.java
@@ -5,10 +5,17 @@ import lombok.RequiredArgsConstructor;
 import team.incude.gsmc.v2.domain.evidence.application.port.EvidencePersistencePort;
 import team.incude.gsmc.v2.domain.evidence.domain.Evidence;
 import team.incude.gsmc.v2.domain.evidence.exception.EvidenceNotFoundException;
+import team.incude.gsmc.v2.domain.evidence.domain.Evidence;
 import team.incude.gsmc.v2.domain.evidence.persistence.mapper.EvidenceMapper;
 import team.incude.gsmc.v2.domain.evidence.persistence.repository.EvidenceJpaRepository;
 import team.incude.gsmc.v2.global.annotation.PortDirection;
 import team.incude.gsmc.v2.global.annotation.adapter.Adapter;
+
+import java.util.List;
+
+import static team.incude.gsmc.v2.domain.evidence.persistence.entity.QEvidenceJpaEntity.evidenceJpaEntity;
+import static team.incude.gsmc.v2.domain.member.persistence.entity.QMemberJpaEntity.memberJpaEntity;
+import static team.incude.gsmc.v2.domain.score.persistence.entity.QScoreJpaEntity.scoreJpaEntity;
 
 @Adapter(direction = PortDirection.OUTBOUND)
 @RequiredArgsConstructor
@@ -28,5 +35,25 @@ public class EvidencePersistenceAdapter implements EvidencePersistencePort {
     @Override
     public Evidence saveEvidence(Evidence evidence) {
         return evidenceMapper.toDomain(evidenceJpaRepository.save(evidenceMapper.toEntity(evidence)));
+    }
+
+    @Override
+    public List<Evidence> findEvidencesByEmail(String email) {
+        return jpaQueryFactory
+                .selectFrom(evidenceJpaEntity)
+                .leftJoin(evidenceJpaEntity.score, scoreJpaEntity).fetchJoin()
+                .leftJoin(scoreJpaEntity.member, memberJpaEntity).fetchJoin()
+                .where(
+                        memberJpaEntity.email.eq(email)
+                )
+                .fetch()
+                .stream()
+                .map(evidenceMapper::toDomain)
+                .toList();
+    }
+
+    @Override
+    public void deleteEvidenceByEvidenceId(Long evidenceId) {
+        evidenceJpaRepository.deleteById(evidenceId);
     }
 }
