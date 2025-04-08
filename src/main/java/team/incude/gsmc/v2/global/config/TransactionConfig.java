3bd629ca2d6f32d909ecdfe20d15b6aad693e2f0
KIM TAEEUN <snowykte0426@naver.com>
2025-04-08 16:05:32 +0900

    add: 트랜젝션 관리 활성화

---
+++ src/main/java/team/incude/gsmc/v2/global/config/TransactionConfig.java
@@ -0,0 +1,9 @@
+package team.incude.gsmc.v2.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+public class TransactionConfig {
+}
