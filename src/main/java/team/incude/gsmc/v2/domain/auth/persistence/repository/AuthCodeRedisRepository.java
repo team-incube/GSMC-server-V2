9fd223bd99823411498ca0c57077409a4add0899
jihoonwjj <wangjajihooon@gmail.com>
2025-04-10 00:41:58 +0900

    add: AuthCodeRedisRepository 추가

---
+++ src/main/java/team/incude/gsmc/v2/domain/auth/persistence/repository/AuthCodeRedisRepository.java
@@ -0,0 +1,15 @@
+package team.incude.gsmc.v2.domain.auth.persistence.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import team.incude.gsmc.v2.domain.auth.persistence.entity.AuthCodeRedisEntity;
+
+@Repository
+public interface AuthCodeRedisRepository extends CrudRepository<AuthCodeRedisEntity, String> {
+
+    AuthCodeRedisEntity findByAuthCode(String code);
+
+    Boolean existsByCode(String code);
+
+    void deleteByCode(String code);
+}
