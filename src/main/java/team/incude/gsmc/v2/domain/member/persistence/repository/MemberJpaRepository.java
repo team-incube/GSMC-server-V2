44013434c8c7efe6dc1cf2bd1c418b8e93378a85
jihoonwjj <wangjajihooon@gmail.com>
2025-04-10 00:49:50 +0900

    add: findByEmail 메소드 추가

--- src/main/java/team/incude/gsmc/v2/domain/member/persistence/repository/MemberJpaRepository.java
+++ src/main/java/team/incude/gsmc/v2/domain/member/persistence/repository/MemberJpaRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import team.incude.gsmc.v2.domain.member.persistence.entity.MemberJpaEntity;
 
+import java.util.Optional;
+
 @Repository
 public interface MemberJpaRepository extends JpaRepository<MemberJpaEntity, Long> {
+    Optional<MemberJpaEntity> findByEmail(String email);
 }
