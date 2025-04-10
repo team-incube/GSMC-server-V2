d64d54b2be16749137a831ba6c56865b9ccccda7
suuuuuuminnnnnn <s24035@gsm.hs.kr>
2025-04-10 20:14:20 +0900

    chore: 불필요한 import 삭제

--- src/main/java/team/incude/gsmc/v2/domain/evidence/exception/EvidenceNotFoundException.java
+++ src/main/java/team/incude/gsmc/v2/domain/evidence/exception/EvidenceNotFoundException.java
@@ -1,6 +1,5 @@
 package team.incude.gsmc.v2.domain.evidence.exception;
 
-import team.incude.gsmc.v2.domain.evidence.domain.Evidence;
 import team.incude.gsmc.v2.global.error.ErrorCode;
 import team.incude.gsmc.v2.global.error.exception.GsmcException;
 
