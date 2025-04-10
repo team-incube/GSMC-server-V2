8f6f889445012c5a1a6ae6e22669db4b44ac9253
suuuuuuminnnnnn <s24035@gsm.hs.kr>
2025-04-10 20:15:16 +0900

    update: findEvidenceByEvidenceId 추가

--- src/main/java/team/incude/gsmc/v2/domain/evidence/application/port/EvidencePersistencePort.java
+++ src/main/java/team/incude/gsmc/v2/domain/evidence/application/port/EvidencePersistencePort.java
@@ -1,6 +1,6 @@
 package team.incude.gsmc.v2.domain.evidence.application.port;
 
-import team.incude.gsmc.v2.domain.evidence.domain.Evidence;
+import team.incude.gsmc.v2.domain.evidence.domain.OtherEvidence;
 import team.incude.gsmc.v2.global.annotation.PortDirection;
 import team.incude.gsmc.v2.global.annotation.port.Port;
 
