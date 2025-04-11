fe9b89f0a400a90a07b6365e00ee7b3aef3028d6
suuuuuuminnnnnn <s24035@gsm.hs.kr>
2025-04-11 19:59:09 +0900

    update: 기타 증빙자료 persistence Port/Adapter 구현

--- src/main/java/team/incude/gsmc/v2/domain/evidence/persistence/OtherEvidencePersistenceAdapter.java
+++ src/main/java/team/incude/gsmc/v2/domain/evidence/persistence/OtherEvidencePersistenceAdapter.java
@@ -3,12 +3,21 @@ package team.incude.gsmc.v2.domain.evidence.persistence;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import team.incude.gsmc.v2.domain.evidence.application.port.OtherEvidencePersistencePort;
-import team.incude.gsmc.v2.domain.evidence.domain.Evidence;
 import team.incude.gsmc.v2.domain.evidence.domain.OtherEvidence;
+import team.incude.gsmc.v2.domain.evidence.domain.constant.EvidenceType;
+import team.incude.gsmc.v2.domain.evidence.persistence.entity.OtherEvidenceJpaEntity;
 import team.incude.gsmc.v2.domain.evidence.persistence.mapper.OtherEvidenceMapper;
 import team.incude.gsmc.v2.domain.evidence.persistence.repository.OtherEvidenceJpaRepository;
+import team.incude.gsmc.v2.domain.member.domain.Member;
+import team.incude.gsmc.v2.domain.member.persistence.mapper.MemberMapper;
 import team.incude.gsmc.v2.global.annotation.PortDirection;
 import team.incude.gsmc.v2.global.annotation.adapter.Adapter;
+
+import java.util.List;
+
+import static team.incude.gsmc.v2.domain.evidence.persistence.entity.QEvidenceJpaEntity.evidenceJpaEntity;
+import static team.incude.gsmc.v2.domain.evidence.persistence.entity.QOtherEvidenceJpaEntity.otherEvidenceJpaEntity;
+import static team.incude.gsmc.v2.domain.score.persistence.entity.QScoreJpaEntity.scoreJpaEntity;
 
 @Adapter(direction = PortDirection.OUTBOUND)
 @RequiredArgsConstructor
@@ -17,9 +26,55 @@ public class OtherEvidencePersistenceAdapter implements OtherEvidencePersistence
     private final OtherEvidenceJpaRepository otherEvidenceJpaRepository;
     private final JPAQueryFactory jpaQueryFactory;
     private final OtherEvidenceMapper otherEvidenceMapper;
+    private final MemberMapper memberMapper;
 
     @Override
     public OtherEvidence saveOtherEvidence(OtherEvidence otherEvidence) {
         return otherEvidenceMapper.toDomain(otherEvidenceJpaRepository.save(otherEvidenceMapper.toEntity(otherEvidence)));
+    }
+
+    @Override
+    public List<OtherEvidence> findOtherEvidenceByMember(Member member) {
+        return jpaQueryFactory
+                .selectFrom(otherEvidenceJpaEntity)
+                .leftJoin(otherEvidenceJpaEntity.evidence, evidenceJpaEntity).fetchJoin()
+                .leftJoin(evidenceJpaEntity.score, scoreJpaEntity).fetchJoin()
+                .where(scoreJpaEntity.member.eq(memberMapper.toEntity(member)))
+                .fetch()
+                .stream()
+                .map(otherEvidenceMapper::toDomain)
+                .toList();
+    }
+
+    @Override
+    public OtherEvidence findOtherEvidenceByEvidenceId(Long evidenceId) {
+        OtherEvidenceJpaEntity jpaEntity = jpaQueryFactory
+                .selectFrom(otherEvidenceJpaEntity)
+                .leftJoin(otherEvidenceJpaEntity.evidence, evidenceJpaEntity).fetchJoin()
+                .where(evidenceJpaEntity.id.eq(evidenceId))
+                .fetchOne();
+
+        return otherEvidenceMapper.toDomain(jpaEntity);
+    }
+
+    @Override
+    public List<OtherEvidence> findOtherEvidenceByMemberAndType(Member member, EvidenceType evidenceType) {
+        return List.of();
+    }
+
+    @Override
+    public void deleteOtherEvidenceById(Long evidenceId) {
+        otherEvidenceJpaRepository.deleteById(evidenceId);
+    }
+
+    @Override
+    public Boolean existsOtherEvidenceByEvidenceId(Long evidenceId) {
+        Integer result = jpaQueryFactory
+                .selectOne()
+                .from(otherEvidenceJpaEntity)
+                .where(otherEvidenceJpaEntity.id.eq(evidenceId))
+                .fetchOne();
+
+        return result != null;
     }
 }
