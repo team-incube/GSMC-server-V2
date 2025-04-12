e0f29137cb7f739d39d2b5d76373c74a97500ed7
suuuuuuminnnnnn <s24035@gsm.hs.kr>
2025-04-12 13:13:44 +0900

    chore: 패키지 경로 변경

--- src/main/java/team/incude/gsmc/v2/domain/evidence/application/EvidenceApplicationAdapter.java
+++ src/main/java/team/incude/gsmc/v2/domain/evidence/application/EvidenceApplicationAdapter.java
@@ -3,10 +3,10 @@ package team.incude.gsmc.v2.domain.evidence.application;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.multipart.MultipartFile;
 import team.incude.gsmc.v2.domain.evidence.application.port.EvidenceApplicationPort;
-import team.incude.gsmc.v2.domain.evidence.application.usecase.*;
+import team.incude.gsmc.v2.domain.evidence.application.usecase.DeleteEvidenceUseCase;
 import team.incude.gsmc.v2.domain.evidence.domain.constant.EvidenceType;
 import team.incude.gsmc.v2.domain.evidence.domain.constant.ReviewStatus;
-import team.incude.gsmc.v2.domain.evidence.presentation.data.GetEvidencesResponse;
+import team.incude.gsmc.v2.domain.evidence.presentation.data.response.GetEvidencesResponse;
 import team.incude.gsmc.v2.global.annotation.PortDirection;
 import team.incude.gsmc.v2.global.annotation.adapter.Adapter;
 
@@ -17,45 +17,47 @@ import java.util.List;
 public class EvidenceApplicationAdapter implements EvidenceApplicationPort {
 
     private final DeleteEvidenceUseCase deleteEvidenceUseCase;
-    private final FindEvidenceByCurrentUserUseCase findEvidenceByCurrentUserUseCase;
-    private final FindEvidenceByEmailUseCase findEvidenceByEmailUseCase;
-    private final SubmitActivityEvidenceUseCase submitActivityEvidenceUseCase;
-    private final SubmitOtherEvidenceUseCase submitOtherEvidenceUseCase;
-    private final SubmitReadingEvidenceUseCase submitReadingEvidenceUseCase;
-    private final UpdateHumanitiesEvidenceByCurrentUserUseCase updateHumanitiesEvidenceByCurrentUserUseCase;
-    private final UpdateMajorEvidenceByCurrentUserUseCase updateMajorEvidenceByCurrentUserUseCase;
-    private final UpdateOtherEvidenceByCurrentUserUseCase updateOtherEvidenceByCurrentUserUseCase;
-    private final UpdateReadingEvidenceByCurrentUserUseCase updateReadingEvidenceByCurrentUserUseCase;
-    private final UpdateReviewStatusUseCase updateReviewStatusUseCase;
+//    private final FindEvidenceByCurrentUserUseCase findEvidenceByCurrentUserUseCase;
+//    private final FindEvidenceByEmailUseCase findEvidenceByEmailUseCase;
+//    private final SubmitActivityEvidenceUseCase submitActivityEvidenceUseCase;
+//    private final SubmitOtherEvidenceUseCase submitOtherEvidenceUseCase;
+//    private final SubmitReadingEvidenceUseCase submitReadingEvidenceUseCase;
+//    private final UpdateHumanitiesEvidenceByCurrentUserUseCase updateHumanitiesEvidenceByCurrentUserUseCase;
+//    private final UpdateMajorEvidenceByCurrentUserUseCase updateMajorEvidenceByCurrentUserUseCase;
+//    private final UpdateOtherEvidenceByCurrentUserUseCase updateOtherEvidenceByCurrentUserUseCase;
+//    private final UpdateReadingEvidenceByCurrentUserUseCase updateReadingEvidenceByCurrentUserUseCase;
+//    private final UpdateReviewStatusUseCase updateReviewStatusUseCase;
+
+    // TODO: 추후 usecase 구현시 변경예정
 
     @Override
     public List<GetEvidencesResponse> findEvidenceByCurrentUser() {
-        return findEvidenceByCurrentUserUseCase.execute();
+        return List.of();
     }
 
     @Override
     public List<GetEvidencesResponse> findEvidenceByEmail(String email) {
-        return findEvidenceByEmailUseCase.execute(email);
+        return List.of();
     }
 
     @Override
     public void updateMajorEvidenceByCurrentUser(Long evidenceId, String title, String content, MultipartFile file) {
-        updateMajorEvidenceByCurrentUserUseCase.execute(evidenceId, title, content, file);
+
     }
 
     @Override
     public void updateHumanitiesEvidenceByCurrentUser(Long evidenceId, String title, String content, MultipartFile file) {
-        updateHumanitiesEvidenceByCurrentUserUseCase.execute(evidenceId, title, content, file);
+
     }
 
     @Override
     public void updateReadingEvidenceByCurrentUser(Long evidenceId, String title, String author, String content, int page) {
-        updateReadingEvidenceByCurrentUserUseCase.execute(evidenceId, title, author, content, page);
+
     }
 
     @Override
     public void updateOtherEvidenceByCurrentUser(Long evidenceId, MultipartFile file) {
-        updateOtherEvidenceByCurrentUserUseCase.execute(evidenceId, file);
+
     }
 
     @Override
@@ -65,21 +67,21 @@ public class EvidenceApplicationAdapter implements EvidenceApplicationPort {
 
     @Override
     public void submitActivityEvidence(String categoryName, String title, String content, MultipartFile file, EvidenceType activityType) {
-        submitActivityEvidenceUseCase.execute(categoryName, title, content, file, activityType);
+
     }
 
     @Override
     public void submitReadingEvidence(String title, String author, int page, String content) {
-        submitReadingEvidenceUseCase.execute(title, author, page, content);
+
     }
 
     @Override
     public void submitOtherEvidence(String categoryName, MultipartFile file) {
-        submitOtherEvidenceUseCase.execute(categoryName, file);
+
     }
 
     @Override
     public void updateReviewStatus(Long evidenceId, ReviewStatus reviewStatus) {
-        updateReviewStatusUseCase.execute(evidenceId, reviewStatus);
+
     }
 }
