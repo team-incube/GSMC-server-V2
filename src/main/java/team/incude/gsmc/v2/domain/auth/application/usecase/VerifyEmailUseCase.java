7e7725e86db01c31e0e3bdee773edcf6c4147e6f
jihoonwjj <wangjajihooon@gmail.com>
2025-04-14 17:45:53 +0900

    update: Service 추가 및 UseCase를 인터페이스로 변경

--- src/main/java/team/incude/gsmc/v2/domain/auth/application/usecase/VerifyEmailUseCase.java
+++ src/main/java/team/incude/gsmc/v2/domain/auth/application/usecase/VerifyEmailUseCase.java
@@ -1,34 +1,5 @@
 package team.incude.gsmc.v2.domain.auth.application.usecase;
 
-import lombok.RequiredArgsConstructor;
-import team.incude.gsmc.v2.domain.auth.application.port.AuthCodePersistencePort;
-import team.incude.gsmc.v2.domain.auth.application.port.AuthenticationPersistencePort;
-import team.incude.gsmc.v2.domain.auth.domain.AuthCode;
-import team.incude.gsmc.v2.domain.auth.domain.Authentication;
-import team.incude.gsmc.v2.domain.auth.exception.VerificationInvalidException;
-import team.incude.gsmc.v2.global.annotation.usecase.UseCase;
-
-@UseCase
-@RequiredArgsConstructor
-public class VerifyEmailUseCase {
-
-    private final AuthCodePersistencePort authCodePersistencePort;
-    private final AuthenticationPersistencePort authenticationPersistencePort;
-
-    public void execute(String code) {
-        if(!authCodePersistencePort.existsAuthCodeByCode(code)) {
-            throw new VerificationInvalidException();
-        }
-
-        AuthCode authCode = authCodePersistencePort.findAuthCodeByCode(code);
-        authCodePersistencePort.deleteAuthCodeByCode(code);
-        Authentication authentication = authenticationPersistencePort.findAuthenticationByEmail(authCode.getEmail());
-        Authentication updatedAuth = Authentication.builder()
-                .email(authCode.getEmail())
-                .attemptCount(authentication.getAttemptCount())
-                .verified(true)
-                .ttl(authentication.getTtl())
-                .build();
-        authenticationPersistencePort.saveAuthentication(updatedAuth);
-    }
+public interface VerifyEmailUseCase {
+    void execute(String code);
 }
