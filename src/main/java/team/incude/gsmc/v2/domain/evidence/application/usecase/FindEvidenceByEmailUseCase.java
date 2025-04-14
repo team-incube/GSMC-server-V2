7795a25580f598c0b1d72fa807089858b0678199
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-04-14 19:59:18 +0900

    delete: 증빙자료 조회 서비스 로직 오버로딩으로 변경하여 삭제

--- src/main/java/team/incude/gsmc/v2/domain/evidence/application/usecase/FindEvidenceByEmailUseCase.java
+++
@@ -1,7 +0,0 @@
-package team.incude.gsmc.v2.domain.evidence.application.usecase;
-
-import team.incude.gsmc.v2.domain.evidence.presentation.data.response.GetEvidencesResponse;
-
-public interface FindEvidenceByEmailUseCase {
-    GetEvidencesResponse execute(String email);
-}
