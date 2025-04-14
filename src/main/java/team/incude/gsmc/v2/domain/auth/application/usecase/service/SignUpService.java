b75ac2c0e32806820cf7f05f3b3a84b0437a5965
jihoonwjj <wangjajihooon@gmail.com>
2025-04-14 20:53:32 +0900

    update: 사용안하는 import 제거 및 Transactional 어노테이션 추가

--- src/main/java/team/incude/gsmc/v2/domain/auth/application/usecase/service/SignUpService.java
+++ src/main/java/team/incude/gsmc/v2/domain/auth/application/usecase/service/SignUpService.java
@@ -3,22 +3,20 @@ package team.incude.gsmc.v2.domain.auth.application.usecase.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import team.incude.gsmc.v2.domain.auth.application.port.AuthenticationPersistencePort;
 import team.incude.gsmc.v2.domain.auth.application.usecase.SignUpUseCase;
-import team.incude.gsmc.v2.domain.auth.domain.Authentication;
 import team.incude.gsmc.v2.domain.auth.exception.EmailFormatInvalidException;
 import team.incude.gsmc.v2.domain.auth.exception.MemberExistException;
-import team.incude.gsmc.v2.domain.auth.exception.MemberForbiddenException;
 import team.incude.gsmc.v2.domain.member.application.port.MemberPersistencePort;
 import team.incude.gsmc.v2.domain.member.application.port.StudentDetailPersistencePort;
 import team.incude.gsmc.v2.domain.member.domain.Member;
 import team.incude.gsmc.v2.domain.member.domain.StudentDetail;
 import team.incude.gsmc.v2.domain.member.domain.constant.MemberRole;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class SignUpService implements SignUpUseCase {
 
@@ -27,7 +25,7 @@ public class SignUpService implements SignUpUseCase {
     private final AuthenticationPersistencePort authenticationPersistencePort;
     private final StudentDetailPersistencePort studentDetailPersistencePort;
 
-    public void execute(String name, String email, String password) {
+    public void execute(String email, String password, String name) {
 //        Authentication authentication = authenticationPersistencePort.findAuthenticationByEmail(email);
 //        if (authentication == null || Boolean.FALSE.equals(authentication.getVerified())) {
 //            throw new MemberForbiddenException();
@@ -45,6 +43,8 @@ public class SignUpService implements SignUpUseCase {
                 .role(MemberRole.ROLE_STUDENT)
                 .build();
 
+        memberPersistencePort.saveMember(member);
+
         StudentDetail studentDetail = StudentDetail.builder()
                 .id(existedStudentDetail.getId())
                 .member(member)
@@ -59,13 +59,12 @@ public class SignUpService implements SignUpUseCase {
     }
 
     private String parsingEmail(String email) {
-        Pattern pattern = Pattern.compile("s(\\d{5})@gsm\\.hs\\.kr");
-        Matcher matcher = pattern.matcher(email);
 
-        if (matcher.find()) {
-            return matcher.group(1);
-        } else {
+        if (email == null | !email.startsWith("s") || !email.endsWith("@gsm.hs.kr")) {
             throw new EmailFormatInvalidException();
         }
+
+        String studentId = email.substring(1, email.indexOf("@"));
+        return studentId;
     }
 }
