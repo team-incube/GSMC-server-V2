d80f3caabb9e098b1eaad52c7537ec2af07e7ca2
jihoonwjj <wangjajihooon@gmail.com>
2025-04-15 00:58:38 +0900

    update: @NotBlank 어노테이션 추가

--- src/main/java/team/incude/gsmc/v2/domain/auth/presentation/data/request/SignUpRequest.java
+++ src/main/java/team/incude/gsmc/v2/domain/auth/presentation/data/request/SignUpRequest.java
@@ -5,7 +5,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record SignUpRequest(
-        @Email @Size(max=16) String email,
+        @Email @NotBlank @Size(max=16) String email,
         @NotBlank @Size(max=30) String password,
         @NotBlank @Size(max=20) String name
 ) {
