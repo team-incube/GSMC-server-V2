2aafbf21cd536a3f62dc6a763e3fcaa73820aacd
jihoonwjj <wangjajihooon@gmail.com>
2025-04-14 17:41:09 +0900

    refactor: value값 변경

--- src/main/java/team/incude/gsmc/v2/global/thirdparty/email/EmailConfig.java
+++ src/main/java/team/incude/gsmc/v2/global/thirdparty/email/EmailConfig.java
@@ -1,6 +1,7 @@
 package team.incude.gsmc.v2.global.thirdparty.email;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
@@ -10,15 +11,16 @@ import java.util.Properties;
 @Configuration
 public class EmailConfig {
 
-    @Value("${email.host}")
+    @Value("${mail.host}")
     private String host;
-    @Value("${email.port}")
+    @Value("${mail.port}")
     private int port;
-    @Value("${email.username}")
+    @Value("${mail.username}")
     private String username;
-    @Value("${email.password}")
+    @Value("${mail.password}")
     private String password;
 
+    @Bean
     public JavaMailSender emailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
         mailSender.setHost(host);
