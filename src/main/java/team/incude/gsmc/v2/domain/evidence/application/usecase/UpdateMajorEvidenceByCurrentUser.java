885f015577db75c8766358eaba228ea49692fe25
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-04-14 20:00:07 +0900

    delete: 활동 영역 수정 서비스 결합으로 삭제

--- src/main/java/team/incude/gsmc/v2/domain/evidence/application/usecase/UpdateMajorEvidenceByCurrentUser.java
+++
@@ -1,7 +0,0 @@
-package team.incude.gsmc.v2.domain.evidence.application.usecase;
-
-import org.springframework.web.multipart.MultipartFile;
-
-public interface UpdateMajorEvidenceByCurrentUser {
-    void execute(Long evidenceId, String title, String content, MultipartFile file);
-}
