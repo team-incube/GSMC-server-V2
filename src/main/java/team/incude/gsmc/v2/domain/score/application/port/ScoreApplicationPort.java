a44f65d737b655e9b5537f22628a7c0f9baadfb4
KIM TAEEUN <snowykte0426@naver.com>
2025-04-14 17:49:28 +0900

    add: 점수 관련 API 비즈니스 로직 필요 메서드들 선언

--- src/main/java/team/incude/gsmc/v2/domain/score/application/port/ScoreApplicationPort.java
+++ src/main/java/team/incude/gsmc/v2/domain/score/application/port/ScoreApplicationPort.java
@@ -1,9 +1,18 @@
 package team.incude.gsmc.v2.domain.score.application.port;
 
+import team.incude.gsmc.v2.domain.score.presentation.data.response.GetScoreResponse;
 import team.incude.gsmc.v2.global.annotation.PortDirection;
 import team.incude.gsmc.v2.global.annotation.port.Port;
 
 @Port(direction = PortDirection.INBOUND)
 public interface ScoreApplicationPort {
+    GetScoreResponse findCurrentScore();
 
+    GetScoreResponse findScoreByEmail(String email);
+
+    void updateCurrentScore(String categoryName, Integer value);
+
+    void updateScoreByEmail(String email, String categoryName, Integer value);
+
+    // void simulateScore(GetScoreSimulateRequest request);
 }
