ca89b6eb6c0e8528bdc619f20372fd0834239599
jihoonwjj <wangjajihooon@gmail.com>
2025-04-14 17:46:57 +0900

    update: Id import 변경

--- src/main/java/team/incude/gsmc/v2/global/security/jwt/entity/RefreshTokenRedisEntity.java
+++ src/main/java/team/incude/gsmc/v2/global/security/jwt/entity/RefreshTokenRedisEntity.java
@@ -1,9 +1,9 @@
 package team.incude.gsmc.v2.global.security.jwt.entity;
 
-import jakarta.persistence.Id;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
 import org.springframework.data.redis.core.TimeToLive;
 import org.springframework.data.redis.core.index.Indexed;
