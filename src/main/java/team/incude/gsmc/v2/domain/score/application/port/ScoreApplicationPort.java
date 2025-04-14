93080a855869ce3760e2ff703b992b8abd3b1446
KIM TAEEUN <snowykte0426@naver.com>
2025-04-14 17:29:09 +0900

    add: 점수 관련 비즈니스 로직 Port 선언

---
+++ src/main/java/team/incude/gsmc/v2/domain/score/application/port/ScoreApplicationPort.java
@@ -0,0 +1,9 @@
+package team.incude.gsmc.v2.domain.score.application.port;
+
+import team.incude.gsmc.v2.global.annotation.PortDirection;
+import team.incude.gsmc.v2.global.annotation.port.Port;
+
+@Port(direction = PortDirection.INBOUND)
+public interface ScoreApplicationPort {
+
+}
