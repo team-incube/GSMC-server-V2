857e46a699d55b0e9e709b1719a917d2a058ba5f
jihoonwjj <wangjajihooon@gmail.com>
2025-04-16 00:45:37 +0900

    refactor: import 구조 변경

--- src/main/java/team/incude/gsmc/v2/global/security/jwt/persistence/repository/RefreshTokenRedisRepository.java
+++ src/main/java/team/incude/gsmc/v2/global/security/jwt/persistence/repository/RefreshTokenRedisRepository.java
@@ -1,8 +1,8 @@
-package team.incude.gsmc.v2.global.security.jwt.repository;
+package team.incude.gsmc.v2.global.security.jwt.persistence.repository;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-import team.incude.gsmc.v2.global.security.jwt.entity.RefreshTokenRedisEntity;
+import team.incude.gsmc.v2.global.security.jwt.persistence.entity.RefreshTokenRedisEntity;
 
 @Repository
 public interface RefreshTokenRedisRepository extends CrudRepository<RefreshTokenRedisEntity, String> {
