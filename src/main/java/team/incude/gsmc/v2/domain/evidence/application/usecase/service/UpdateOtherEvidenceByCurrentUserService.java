e48a5528ffa05255c2d986c23954e941e002fd8a
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-04-15 16:59:18 +0900

    add: ActivityEvidence 생성 서비스 구현

---
+++ src/main/java/team/incude/gsmc/v2/domain/evidence/application/usecase/service/UpdateOtherEvidenceByCurrentUserService.java
@@ -0,0 +1,80 @@
+package team.incude.gsmc.v2.domain.evidence.application.usecase.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+import team.incude.gsmc.v2.domain.evidence.application.port.EvidencePersistencePort;
+import team.incude.gsmc.v2.domain.evidence.application.port.OtherEvidencePersistencePort;
+import team.incude.gsmc.v2.domain.evidence.application.port.S3Port;
+import team.incude.gsmc.v2.domain.evidence.application.usecase.UpdateOtherEvidenceByCurrentUserUseCase;
+import team.incude.gsmc.v2.domain.evidence.domain.Evidence;
+import team.incude.gsmc.v2.domain.evidence.domain.OtherEvidence;
+import team.incude.gsmc.v2.domain.evidence.domain.constant.ReviewStatus;
+import team.incude.gsmc.v2.domain.evidence.exception.OtherEvidenceNotFoundException;
+import team.incude.gsmc.v2.global.thirdparty.aws.exception.S3UploadFailedException;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class UpdateOtherEvidenceByCurrentUserService implements UpdateOtherEvidenceByCurrentUserUseCase {
+
+    private final OtherEvidencePersistencePort otherEvidencePersistencePort;
+    private final EvidencePersistencePort evidencePersistencePort;
+    private final S3Port s3Port;
+
+
+    @Override
+    @Transactional
+    public void execute(Long evidenceId, MultipartFile file) {
+        Evidence evidence = evidencePersistencePort.findEvidenceById(evidenceId);
+        validateNotExistsOther(evidenceId);
+
+        Evidence newEvidence = createEvidence(evidence);
+        OtherEvidence newOtherEvidence = createOtherEvidence(newEvidence, file);
+        saveOtherEvidence(newOtherEvidence);
+    }
+
+    private void validateNotExistsOther(Long evidenceId) {
+        if (otherEvidencePersistencePort.existsOtherEvidenceByEvidenceId(evidenceId)) {
+            throw new OtherEvidenceNotFoundException();
+        }
+    }
+
+    private Evidence createEvidence(Evidence evidence) {
+        return Evidence.builder()
+                .id(evidence.getId())
+                .score(evidence.getScore())
+                .reviewStatus(ReviewStatus.PENDING)
+                .evidenceType(evidence.getEvidenceType())
+                .createdAt(evidence.getCreatedAt())
+                .updatedAt(evidence.getUpdatedAt())
+                .build();
+    }
+
+    private OtherEvidence createOtherEvidence(Evidence evidence, MultipartFile file) {
+        return OtherEvidence.builder()
+                .id(evidence)
+                .fileUri(uploadFile(file))
+                .build();
+    }
+
+    private String uploadFile(MultipartFile file) {
+        if (file == null || file.isEmpty()) return null;
+
+        try {
+            return s3Port.uploadFile(
+                    UUID.randomUUID().toString(),
+                    file.getInputStream()
+            ).join();
+        } catch (IOException e) {
+            throw new S3UploadFailedException();
+        }
+    }
+
+    private void saveOtherEvidence(OtherEvidence otherEvidence) {
+        otherEvidencePersistencePort.saveOtherEvidence(otherEvidence);
+    }
+}
