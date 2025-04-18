82fb7ec4e66b43daf20d951f18262faba83bfdf3
황지훈 <s24072@gsm.hs.kr>
2025-04-18 23:30:01 +0900

    add: /change-password 엔드포인트 구현

--- src/main/java/team/incude/gsmc/v2/domain/auth/presentation/data/AuthWebAdapter.java
+++ src/main/java/team/incude/gsmc/v2/domain/auth/presentation/data/AuthWebAdapter.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import team.incude.gsmc.v2.domain.auth.application.port.AuthApplicationPort;
 import team.incude.gsmc.v2.domain.auth.presentation.data.request.*;
 import team.incude.gsmc.v2.domain.auth.presentation.data.response.AuthTokenResponse;
+import team.incude.gsmc.v2.domain.auth.presentation.data.request.ChangePasswordRequest;
 
 @RequestMapping("/api/v2/auth")
 @RequiredArgsConstructor
@@ -18,32 +19,38 @@ public class AuthWebAdapter {
     private final AuthApplicationPort authApplicationPort;
 
     @PostMapping("/signin")
-    public ResponseEntity<AuthTokenResponse> signIn(@Valid @RequestBody SignInRequest signInRequest) {
-        return ResponseEntity.status(HttpStatus.OK).body(authApplicationPort.signIn(signInRequest.email(), signInRequest.password()));
+    public ResponseEntity<AuthTokenResponse> signIn(@Valid @RequestBody SignInRequest request) {
+        return ResponseEntity.status(HttpStatus.OK).body(authApplicationPort.signIn(request.email(), request.password()));
     }
 
     @PostMapping("/signup")
     @ResponseStatus(HttpStatus.CREATED)
-    public ResponseEntity<Void> signUp(@Valid @RequestBody SignUpRequest signUpRequest) {
-        authApplicationPort.signUp(signUpRequest.name(), signUpRequest.email(), signUpRequest.password());
+    public ResponseEntity<Void> signUp(@Valid @RequestBody SignUpRequest request) {
+        authApplicationPort.signUp(request.name(), request.email(), request.password());
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
     @PutMapping("/refresh")
-    public ResponseEntity<AuthTokenResponse> refreshToken(@Valid @RequestBody RefreshRequest refreshRequest) {
-        return ResponseEntity.status(HttpStatus.OK).body(authApplicationPort.refresh(refreshRequest.refreshToken()));
+    public ResponseEntity<AuthTokenResponse> refreshToken(@Valid @RequestBody RefreshRequest request) {
+        return ResponseEntity.status(HttpStatus.OK).body(authApplicationPort.refresh(request.refreshToken()));
     }
 
     @PatchMapping("/verify-email")
-    public ResponseEntity<Void> verifyEmail(@Valid @RequestBody VerificationCodeRequest verificationCodeRequest) {
-        authApplicationPort.verifyEmail(verificationCodeRequest.code());
+    public ResponseEntity<Void> verifyEmail(@Valid @RequestBody VerificationCodeRequest request) {
+        authApplicationPort.verifyEmail(request.code());
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
     @PostMapping("/send-email")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public ResponseEntity<Void> sendAuthenticationEmail(@Valid @RequestBody SendEmailRequest sendEmailRequest) throws MessagingException {
-        authApplicationPort.sendAuthenticationEmail(sendEmailRequest.email());
+    public ResponseEntity<Void> sendAuthenticationEmail(@Valid @RequestBody SendEmailRequest request) {
+        authApplicationPort.sendAuthenticationEmail(request.email());
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+    @PatchMapping("/change-password")
+    public ResponseEntity<Void> changePassword(@Valid @RequestBody ChangePasswordRequest request) {
+        authApplicationPort.changePassword(request.email(), request.password());
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
