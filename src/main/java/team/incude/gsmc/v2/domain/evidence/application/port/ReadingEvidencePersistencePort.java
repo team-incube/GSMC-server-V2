07cab782f58f2e47e7322696fb643a14f3394912
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-04-18 10:04:00 +0900

    update: 불필요한 메서드 제거 및 메서드 파라미터 변경

--- src/main/java/team/incude/gsmc/v2/domain/evidence/application/port/ReadingEvidencePersistencePort.java
+++ src/main/java/team/incude/gsmc/v2/domain/evidence/application/port/ReadingEvidencePersistencePort.java
@@ -1,7 +1,6 @@
 package team.incude.gsmc.v2.domain.evidence.application.port;
 
 import team.incude.gsmc.v2.domain.evidence.domain.ReadingEvidence;
-import team.incude.gsmc.v2.domain.member.domain.Member;
 import team.incude.gsmc.v2.global.annotation.PortDirection;
 import team.incude.gsmc.v2.global.annotation.port.Port;
 
@@ -9,13 +8,12 @@ import java.util.List;
 
 @Port(direction = PortDirection.OUTBOUND)
 public interface ReadingEvidencePersistencePort {
-    List<ReadingEvidence> findReadingEvidenceByMember(Member member);
+    List<ReadingEvidence> findReadingEvidenceByEmail(String email);
 
-    ReadingEvidence findReadingEvidenceByEvidenceId(Long evidenceId);
 
     ReadingEvidence saveReadingEvidence(ReadingEvidence readingEvidence);
 
-    List<ReadingEvidence> findReadingEvidenceByMemberAndTypeAndTitle(Member member);
+    List<ReadingEvidence> findReadingEvidenceByEmailAndTitleAndTypeAndGradeAndClassNumber(String email, String title, EvidenceType evidenceType, Integer grade, Integer classNumber);
 
     void deleteReadingEvidenceById(Long evidenceId);
 
