59858a2360e7d7998de2a46cb60431d65766c281
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-04-19 22:01:02 +0900

    add: CreateActivityEvidenceService 추가

--- src/main/java/team/incude/gsmc/v2/domain/evidence/application/usecase/service/CreateActivityEvidenceService.java
+++ src/main/java/team/incude/gsmc/v2/domain/evidence/application/usecase/service/CreateActivityEvidenceService.java
@@ -2,14 +2,24 @@ package team.incude.gsmc.v2.domain.evidence.application.usecase.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 import team.incude.gsmc.v2.domain.evidence.application.port.ActivityEvidencePersistencePort;
 import team.incude.gsmc.v2.domain.evidence.application.port.S3Port;
 import team.incude.gsmc.v2.domain.evidence.application.usecase.CreateActivityEvidenceUseCase;
+import team.incude.gsmc.v2.domain.evidence.domain.ActivityEvidence;
+import team.incude.gsmc.v2.domain.evidence.domain.Evidence;
 import team.incude.gsmc.v2.domain.evidence.domain.constant.EvidenceType;
+import team.incude.gsmc.v2.domain.evidence.domain.constant.ReviewStatus;
 import team.incude.gsmc.v2.domain.member.domain.Member;
 import team.incude.gsmc.v2.domain.score.application.port.ScorePersistencePort;
+import team.incude.gsmc.v2.domain.score.domain.Score;
 import team.incude.gsmc.v2.global.security.jwt.usecase.service.CurrentMemberProvider;
+import team.incude.gsmc.v2.global.thirdparty.aws.exception.S3UploadFailedException;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -22,7 +32,49 @@ public class CreateActivityEvidenceService implements CreateActivityEvidenceUseC
 
 
     @Override
+    @Transactional
     public void execute(String categoryName, String title, String content, MultipartFile file, EvidenceType activityType) {
         Member member = currentMemberProvider.getCurrentUser();
+        Score score = scorePersistencePort.findScoreByCategoryNameAndMemberEmail(categoryName, member.getEmail());
+
+        score.plusValue(1);
+
+        Evidence evidence = createEvidence(score, activityType);
+        ActivityEvidence activityEvidence = createActivityEvidence(evidence, title, content, file);
+
+        scorePersistencePort.saveScore(score);
+        activityEvidencePersistencePort.saveActivityEvidence(activityEvidence);
+    }
+
+    private Evidence createEvidence(Score score, EvidenceType activityType) {
+        return Evidence.builder()
+                .score(score)
+                .reviewStatus(ReviewStatus.PENDING)
+                .evidenceType(activityType)
+                .createdAt(LocalDateTime.now())
+                .updatedAt(LocalDateTime.now())
+                .build();
+    }
+
+    private ActivityEvidence createActivityEvidence(Evidence evidence, String title, String content, MultipartFile file) {
+        return ActivityEvidence.builder()
+                .id(evidence)
+                .title(title)
+                .content(content)
+                .imageUrl(uploadFile(file))
+                .build();
+    }
+
+    private String uploadFile(MultipartFile file) {
+        if (file == null || file.isEmpty()) return null;
+
+        try {
+            return s3Port.uploadFile(
+                    UUID.randomUUID().toString(),
+                    file.getInputStream()
+            ).join();
+        } catch (IOException e) {
+            throw new S3UploadFailedException();
+        }
     }
 }
