b9b2792ab1efc6c1cad264f96e00160a67b6a40f
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-04-19 13:04:19 +0900

    update: 증빙자료 검색 usecase 인터페이스 수정

--- src/main/java/team/incude/gsmc/v2/domain/evidence/application/usecase/FindEvidenceByEmailAndFilteringTypeAndStatusUseCase.java
+++ src/main/java/team/incude/gsmc/v2/domain/evidence/application/usecase/FindEvidenceByEmailAndFilteringTypeAndStatusUseCase.java
@@ -1,8 +1,9 @@
 package team.incude.gsmc.v2.domain.evidence.application.usecase;
 
 import team.incude.gsmc.v2.domain.evidence.domain.constant.EvidenceType;
+import team.incude.gsmc.v2.domain.evidence.domain.constant.ReviewStatus;
 import team.incude.gsmc.v2.domain.evidence.presentation.data.response.GetEvidencesResponse;
 
-public interface FindEvidenceByFilteringByEmailAndTitleAndTypeUseCase {
-    GetEvidencesResponse execute(String email, String title, EvidenceType evidenceType);
+public interface FindEvidenceByEmailAndFilteringTypeAndStatusUseCase {
+    GetEvidencesResponse execute(String email, EvidenceType type, ReviewStatus status);
 }
