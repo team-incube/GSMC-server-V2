f5e6b8c196375e01fd8a6a313f5fa835929ecb67
KIM TAEEUN <snowykte0426@naver.com>
2025-04-19 20:27:11 +0900

    add: 종합 점수 계산 비즈니스 로직 클래스 구현

---
+++ src/main/java/team/incude/gsmc/v2/domain/score/application/usecase/service/CalculateTotalScoreService.java
@@ -0,0 +1,83 @@
+package team.incude.gsmc.v2.domain.score.application.usecase.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import team.incude.gsmc.v2.domain.score.application.port.ScorePersistencePort;
+import team.incude.gsmc.v2.domain.score.application.usecase.CalculateTotalScoreUseCase;
+import team.incude.gsmc.v2.domain.score.domain.Score;
+import team.incude.gsmc.v2.global.util.SimulateScoreUtil;
+
+import java.util.List;
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
+public class CalculateTotalScoreService implements CalculateTotalScoreUseCase {
+
+    private final ScorePersistencePort scorePersistencePort;
+
+    @Override
+    public void execute(String email) {
+        List<Score> scores = scorePersistencePort.findScoreByMemberEmail(email);
+        Map<String, Float> categoryWeights = scores.stream().map(
+                score -> Map.entry(score.getCategory().getName(), score.getCategory().getWeight())
+        ).reduce(
+                Map.of(),
+                (acc, entry) -> {
+                    acc.put(entry.getKey(), entry.getValue());
+                    return acc;
+                },
+                (acc1, acc2) -> {
+                    acc1.putAll(acc2);
+                    return acc1;
+                }
+        );
+        List<String> categoryNames = scores.stream().map(
+                score -> score.getCategory().getName()
+        ).toList();
+        SimulateScoreUtil.simulateScore(
+                Integer.parseInt(categoryNames.getFirst()),
+                Integer.parseInt(categoryNames.get(1)),
+                Integer.parseInt(categoryNames.get(2)),
+                Integer.parseInt(categoryNames.get(3)),
+                Integer.parseInt(categoryNames.get(4)),
+                Integer.parseInt(categoryNames.get(5)),
+                Integer.parseInt(categoryNames.get(6)),
+                Integer.parseInt(categoryNames.get(7)),
+                Integer.parseInt(categoryNames.get(8)),
+                Integer.parseInt(categoryNames.get(9)),
+                Integer.parseInt(categoryNames.get(10)),
+                Integer.parseInt(categoryNames.get(11)),
+                Integer.parseInt(categoryNames.get(12)),
+                Integer.parseInt(categoryNames.get(13)),
+                Integer.parseInt(categoryNames.get(14)),
+                Integer.parseInt(categoryNames.get(15)),
+                Integer.parseInt(categoryNames.get(16)),
+                Integer.parseInt(categoryNames.get(17)),
+                Integer.parseInt(categoryNames.get(18)),
+                Integer.parseInt(categoryNames.get(19)),
+                Integer.parseInt(categoryNames.get(20)),
+                Boolean.parseBoolean(categoryNames.get(21)),
+                Boolean.parseBoolean(categoryNames.get(22)),
+                Boolean.parseBoolean(categoryNames.get(23)),
+                Integer.parseInt(categoryNames.get(24)),
+                Integer.parseInt(categoryNames.get(25)),
+                Integer.parseInt(categoryNames.get(26)),
+                Integer.parseInt(categoryNames.get(27)),
+                Boolean.parseBoolean(categoryNames.get(28)),
+                Boolean.parseBoolean(categoryNames.get(29)),
+                Integer.parseInt(categoryNames.get(30)),
+                Integer.parseInt(categoryNames.get(31)),
+                Boolean.parseBoolean(categoryNames.get(32)),
+                Integer.parseInt(categoryNames.get(33)),
+                Integer.parseInt(categoryNames.get(34)),
+                Integer.parseInt(categoryNames.get(35)),
+                Integer.parseInt(categoryNames.get(36)),
+                Integer.parseInt(categoryNames.get(37)),
+                Integer.parseInt(categoryNames.get(38)),
+                Integer.parseInt(categoryNames.get(39)),
+                Integer.parseInt(categoryNames.get(40)),
+                categoryWeights
+        );
+    }
+}
