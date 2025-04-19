18c15cd6b1bcbbdb75b2efd37c46fceae1568ab0
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-04-20 00:05:19 +0900

    update: createActivityEvidence 추가

--- src/main/java/team/incude/gsmc/v2/domain/evidence/presentation/EvidenceWebAdapter.java
+++ src/main/java/team/incude/gsmc/v2/domain/evidence/presentation/EvidenceWebAdapter.java
@@ -1,12 +1,24 @@
 package team.incude.gsmc.v2.domain.evidence.presentation;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import team.incude.gsmc.v2.domain.evidence.application.usecase.service.CreateActivityEvidenceService;
+import team.incude.gsmc.v2.domain.evidence.presentation.data.request.CreateActivityEvidenceRequest;
 
 @RestController
 @RequestMapping("/api/v2/evidence")
 @RequiredArgsConstructor
 public class EvidenceWebAdapter {
-    // TODO: 테스트를 위해 생성하였던 클래스입니다, 추후 실제 구현시에도 사용하여주세요
+
+    private final CreateActivityEvidenceService createActivityEvidenceService;
+
+    @PostMapping("/current/activity")
+    public ResponseEntity<Void> createActivityEvidence(@RequestBody CreateActivityEvidenceRequest request) {
+        createActivityEvidenceService.execute(request.categoryName(), request.title(), request.content(), request.file(), request.activityType());
+        return ResponseEntity.ok().build();
+    }
 }
