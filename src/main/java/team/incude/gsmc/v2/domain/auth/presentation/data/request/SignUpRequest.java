c0f59601cdd14ca5e35b054e175b7d8f2a3a4105
황지훈 <s24072@gsm.hs.kr>
2025-04-19 22:24:22 +0900

    delete: email size 어노테이션 제거

--- src/main/java/team/incude/gsmc/v2/domain/auth/presentation/data/request/SignUpRequest.java
+++ src/main/java/team/incude/gsmc/v2/domain/auth/presentation/data/request/SignUpRequest.java
@@ -6,7 +6,7 @@ import jakarta.validation.constraints.Size;
 
 public record SignUpRequest(
         @NotBlank @Size(max=20) String name,
-        @Email @NotBlank @Size(max=16) String email,
+        @Email @NotBlank String email,
         @NotBlank @Size(max=30) String password
 ) {
 }
