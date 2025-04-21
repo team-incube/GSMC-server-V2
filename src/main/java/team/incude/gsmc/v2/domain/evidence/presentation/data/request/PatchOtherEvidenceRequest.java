4d75730cc65aeff3d95567adc0b3b7e4f211ed29
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-04-21 23:47:58 +0900

    delete: PatchOtherEvidenceRequest 삭제

--- src/main/java/team/incude/gsmc/v2/domain/evidence/presentation/data/request/PatchOtherEvidenceRequest.java
+++
@@ -1,8 +0,0 @@
-package team.incude.gsmc.v2.domain.evidence.presentation.data.request;
-
-import org.springframework.web.multipart.MultipartFile;
-
-public record PatchOtherEvidenceRequest(
-        MultipartFile file
-) {
-}
