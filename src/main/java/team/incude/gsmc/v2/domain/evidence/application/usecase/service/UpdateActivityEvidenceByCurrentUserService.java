139f1cfad04dc50d828959c18de17f0c510d9ec8
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-04-21 17:22:23 +0900

    add: 현재 인증된 사용자 활동영역 증빙자료 수정 서비스 구현

---
+++ src/main/java/team/incude/gsmc/v2/domain/evidence/application/usecase/service/UpdateActivityEvidenceByCurrentUserService.java
@@ -0,0 +1,71 @@
+package team.incude.gsmc.v2.domain.evidence.application.usecase.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import team.incude.gsmc.v2.domain.evidence.application.port.ActivityEvidencePersistencePort;
+import team.incude.gsmc.v2.domain.evidence.application.port.EvidencePersistencePort;
+import team.incude.gsmc.v2.domain.evidence.application.port.S3Port;
+import team.incude.gsmc.v2.domain.evidence.application.usecase.UpdateActivityEvidenceByCurrentUserUseCase;
+import team.incude.gsmc.v2.domain.evidence.domain.ActivityEvidence;
+import team.incude.gsmc.v2.domain.evidence.domain.Evidence;
+import team.incude.gsmc.v2.domain.evidence.domain.constant.EvidenceType;
+import team.incude.gsmc.v2.domain.evidence.domain.constant.ReviewStatus;
+import team.incude.gsmc.v2.global.thirdparty.aws.exception.S3UploadFailedException;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class UpdateActivityEvidenceByCurrentUserService implements UpdateActivityEvidenceByCurrentUserUseCase {
+
+    private final EvidencePersistencePort evidencePersistencePort;
+    private final S3Port s3Port;
+    private final ActivityEvidencePersistencePort activityEvidencePersistencePort;
+
+    @Override
+    public void execute(Long evidenceId, String title, String content, MultipartFile file, EvidenceType evidenceType) {
+        Evidence evidence = evidencePersistencePort.findEvidenceById(evidenceId);
+
+        Evidence newEvidence = createEvidence(evidence, evidenceType);
+        String fileUrl = uploadFile(file);
+        ActivityEvidence newActivityEvidence = createActivityEvidence(newEvidence, title, content, fileUrl);
+
+        activityEvidencePersistencePort.saveActivityEvidence(newActivityEvidence);
+    }
+
+    private ActivityEvidence createActivityEvidence(Evidence evidence, String title, String content, String fileUrl) {
+        return ActivityEvidence.builder()
+                .id(evidence)
+                .title(title)
+                .content(content)
+                .imageUrl(fileUrl)
+                .build();
+    }
+
+    private Evidence createEvidence(Evidence evidence, EvidenceType evidenceType) {
+        return Evidence.builder()
+                .id(evidence.getId())
+                .score(evidence.getScore())
+                .reviewStatus(ReviewStatus.PENDING)
+                .evidenceType(evidenceType)
+                .createdAt(evidence.getCreatedAt())
+                .updatedAt(LocalDateTime.now())
+                .build();
+    }
+
+    private String uploadFile(MultipartFile file) {
+        if (file == null || file.isEmpty()) return null;
+
+        try {
+            return s3Port.uploadFile(
+                    UUID.randomUUID().toString(),
+                    file.getInputStream()
+            ).join();
+        } catch (IOException e) {
+            throw new S3UploadFailedException();
+        }
+    }
+}
