9a4cd3fbb0e31d4c39d512603adab45035eeb2d3
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-04-21 17:11:06 +0900

    add: 현재 인증된 사용자 증빙자료 조회 서비스 구현

---
+++ src/main/java/team/incude/gsmc/v2/domain/evidence/application/usecase/service/FindEvidenceByCurrentUserAndTypeService.java
@@ -0,0 +1,97 @@
+package team.incude.gsmc.v2.domain.evidence.application.usecase.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import team.incude.gsmc.v2.domain.evidence.application.port.ActivityEvidencePersistencePort;
+import team.incude.gsmc.v2.domain.evidence.application.port.OtherEvidencePersistencePort;
+import team.incude.gsmc.v2.domain.evidence.application.port.ReadingEvidencePersistencePort;
+import team.incude.gsmc.v2.domain.evidence.application.usecase.FindEvidenceByCurrentUserAndTypeUseCase;
+import team.incude.gsmc.v2.domain.evidence.domain.ActivityEvidence;
+import team.incude.gsmc.v2.domain.evidence.domain.OtherEvidence;
+import team.incude.gsmc.v2.domain.evidence.domain.ReadingEvidence;
+import team.incude.gsmc.v2.domain.evidence.domain.constant.EvidenceType;
+import team.incude.gsmc.v2.domain.evidence.presentation.data.GetActivityEvidenceDto;
+import team.incude.gsmc.v2.domain.evidence.presentation.data.GetOtherEvidenceDto;
+import team.incude.gsmc.v2.domain.evidence.presentation.data.GetReadingEvidenceDto;
+import team.incude.gsmc.v2.domain.evidence.presentation.data.response.GetEvidencesResponse;
+import team.incude.gsmc.v2.domain.member.application.port.StudentDetailPersistencePort;
+import team.incude.gsmc.v2.domain.member.domain.Member;
+import team.incude.gsmc.v2.global.security.jwt.usecase.service.CurrentMemberProvider;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class FindEvidenceByCurrentUserAndTypeService implements FindEvidenceByCurrentUserAndTypeUseCase {
+
+    private final ActivityEvidencePersistencePort activityEvidencePersistencePort;
+    private final ReadingEvidencePersistencePort readingEvidencePersistencePort;
+    private final OtherEvidencePersistencePort otherEvidencePersistencePort;
+    private final CurrentMemberProvider currentMemberProvider;
+    private final StudentDetailPersistencePort studentDetailPersistencePort;
+
+    @Override
+    @Transactional(readOnly = true)
+    public GetEvidencesResponse execute(EvidenceType type) {
+        Member member = currentMemberProvider.getCurrentUser();
+
+        List<ActivityEvidence> activityEvidences = activityEvidencePersistencePort.findActivityEvidenceByEmailAndEvidenceType(member.getEmail(), type);
+
+        List<ActivityEvidence> majorEvidences = filterByType(activityEvidences, type);
+        List<ActivityEvidence> humanitiesEvidences = filterByType(activityEvidences, type);
+        List<ReadingEvidence> readingEvidences = readingEvidencePersistencePort.findReadingEvidenceByEmail(member.getEmail());
+        List<OtherEvidence> otherEvidences = otherEvidencePersistencePort.findOtherEvidenceByEmail(member.getEmail());
+
+        List<GetActivityEvidenceDto> majorEvidenceDtos = createActivityEvidenceDtos(majorEvidences);
+        List<GetActivityEvidenceDto> humanitiesEvidenceDtos = createActivityEvidenceDtos(humanitiesEvidences);
+        List<GetReadingEvidenceDto> readingEvidenceDtos = createReadingEvidenceDtos(readingEvidences);
+        List<GetOtherEvidenceDto> otherEvidenceDtos = createOtherEvidenceDtos(otherEvidences);
+
+        return new GetEvidencesResponse(majorEvidenceDtos, humanitiesEvidenceDtos, readingEvidenceDtos, otherEvidenceDtos);
+    }
+
+    private List<ActivityEvidence> filterByType(List<ActivityEvidence> evidences, EvidenceType type) {
+        return evidences.stream()
+                .filter(e -> e.getId().getEvidenceType().equals(type))
+                .toList();
+    }
+
+    private List<GetActivityEvidenceDto> createActivityEvidenceDtos(List<ActivityEvidence> evidences) {
+        return evidences.stream()
+                .map(e -> new GetActivityEvidenceDto(
+                        e.getId().getId(),
+                        e.getTitle(),
+                        e.getContent(),
+                        e.getImageUrl(),
+                        e.getId().getReviewStatus(),
+                        e.getId().getScore().getCategory().getName()
+                ))
+                .toList();
+    }
+
+    private List<GetOtherEvidenceDto> createOtherEvidenceDtos(List<OtherEvidence> evidences) {
+        return evidences.stream()
+                .map(e -> new GetOtherEvidenceDto(
+                        e.getId().getId(),
+                        e.getFileUri(),
+                        e.getId().getEvidenceType(),
+                        e.getId().getReviewStatus(),
+                        e.getId().getScore().getCategory().getName()
+                ))
+                .toList();
+    }
+
+    private List<GetReadingEvidenceDto> createReadingEvidenceDtos(List<ReadingEvidence> evidences) {
+        return evidences.stream()
+                .map(e -> new GetReadingEvidenceDto(
+                        e.getId().getId(),
+                        e.getTitle(),
+                        e.getAuthor(),
+                        e.getPage(),
+                        e.getContent(),
+                        e.getId().getReviewStatus()
+                ))
+                .toList();
+    }
+}
