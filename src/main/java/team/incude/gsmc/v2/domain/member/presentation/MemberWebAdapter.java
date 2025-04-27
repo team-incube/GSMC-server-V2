129b88f4265eeb1aba948f4dfb347d80ff112069
snowykte0426 <snowykte0426@naver.com>
2025-04-27 13:00:51 +0900

    add: ``member`` 도메인 모델 컨트롤러 선언

---
+++ src/main/java/team/incude/gsmc/v2/domain/member/presentation/MemberWebAdapter.java
@@ -0,0 +1,37 @@
+package team.incude.gsmc.v2.domain.member.presentation;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+
+@RestController
+@RequestMapping("/api/v2/members")
+@RequiredArgsConstructor
+public class MemberWebAdapter {
+
+
+    @GetMapping("/students")
+    public ResponseEntity<Object> getAllStudents() {
+        return ResponseEntity.status(HttpStatus.OK).body(new ArrayList<>());
+    }
+
+    @GetMapping("/students/search")
+    public ResponseEntity<Object> searchStudents() {
+        return ResponseEntity.status(HttpStatus.OK).body(new ArrayList<>());
+    }
+
+    @GetMapping("/students/current")
+    public ResponseEntity<Object> getCurrentStudent() {
+        return ResponseEntity.status(HttpStatus.OK).body(new ArrayList<>());
+    }
+
+    @GetMapping("/students/{studentCode}")
+    public ResponseEntity<Object> getStudent() {
+        return ResponseEntity.status(HttpStatus.OK).body(new ArrayList<>());
+    }
+}
