4f8a28f81830d02e849384af98149588e7b539e0
snowykte0426 <snowykte0426@naver.com>
2025-04-27 13:53:10 +0900

    update: 미사용 의존성 제거

--- src/main/java/team/incude/gsmc/v2/domain/member/application/usecase/service/FindAllMemberService.java
+++ src/main/java/team/incude/gsmc/v2/domain/member/application/usecase/service/FindAllMemberService.java
@@ -3,7 +3,6 @@ package team.incude.gsmc.v2.domain.member.application.usecase.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import team.incude.gsmc.v2.domain.member.application.usecase.FindAllMemberUseCase;
-import team.incude.gsmc.v2.domain.member.persistence.MemberPersistenceAdapter;
 import team.incude.gsmc.v2.domain.member.persistence.StudentDetailPersistenceAdapter;
 import team.incude.gsmc.v2.domain.member.presentation.data.response.GetMemberResponse;
 
