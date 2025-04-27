ffb5305690ead77b4e4e96485ea758b2472cbd1f
snowykte0426 <snowykte0426@naver.com>
2025-04-27 13:10:08 +0900

    update: 경로변수 사용 설정

--- src/main/java/team/incude/gsmc/v2/domain/member/presentation/MemberWebAdapter.java
+++ src/main/java/team/incude/gsmc/v2/domain/member/presentation/MemberWebAdapter.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,7 +32,7 @@ public class MemberWebAdapter {
     }
 
     @GetMapping("/students/{studentCode}")
-    public ResponseEntity<Object> getStudent() {
+    public ResponseEntity<Object> getStudent(@PathVariable(value = "studentCode") String studentCode) {
         return ResponseEntity.status(HttpStatus.OK).body(new ArrayList<>());
     }
 }
