bad91b9b730c9c2200b579ac7dfc993de2dd55f7
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-04-30 14:43:59 +0900

    update: 증빙자료 생성 및 수정 dto @Size 어노테이션 추가

--- src/main/java/team/incude/gsmc/v2/domain/evidence/presentation/data/request/CreateReadingEvidenceRequest.java
+++ src/main/java/team/incude/gsmc/v2/domain/evidence/presentation/data/request/CreateReadingEvidenceRequest.java
@@ -1,11 +1,12 @@
 package team.incude.gsmc.v2.domain.evidence.presentation.data.request;
 
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 public record CreateReadingEvidenceRequest(
-        @NotNull String title,
-        @NotNull String author,
+        @NotNull @Size(max = 100) String title,
+        @NotNull @Size(max = 100) String author,
         @NotNull Integer page,
-        @NotNull String content
+        @NotNull @Size(max = 1500) String content
 ) {
 }
