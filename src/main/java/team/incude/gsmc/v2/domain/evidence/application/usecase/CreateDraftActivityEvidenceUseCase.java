325e5b0bdff40217eac1a449498dd6961fe9ff7e
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-05-02 00:11:42 +0900

    add: 활동 영역 증빙자료 임시 저장 서비스 구현

--- src/main/java/team/incude/gsmc/v2/domain/evidence/application/usecase/CreateDraftActivityEvidenceUseCase.java
+++ src/main/java/team/incude/gsmc/v2/domain/evidence/application/usecase/CreateDraftActivityEvidenceUseCase.java
@@ -1,8 +1,11 @@
 package team.incude.gsmc.v2.domain.evidence.application.usecase;
 
-import team.incude.gsmc.v2.domain.evidence.presentation.data.request.CreateDraftActivityEvidenceReqeust;
+import org.springframework.web.multipart.MultipartFile;
+import team.incude.gsmc.v2.domain.evidence.domain.constant.EvidenceType;
 import team.incude.gsmc.v2.domain.evidence.presentation.data.response.CreateDraftEvidenceResponse;
 
+import java.util.UUID;
+
 public interface CreateDraftActivityEvidenceUseCase {
-    CreateDraftEvidenceResponse execute(CreateDraftActivityEvidenceReqeust request);
+    CreateDraftEvidenceResponse execute(UUID id, String categoryName, String title, String content, MultipartFile file, String imageUrl, EvidenceType activityType);
 }
---
+++ src/main/java/team/incude/gsmc/v2/domain/evidence/application/usecase/service/CreateDraftActivityEvidenceService.java
@@ -0,0 +1,68 @@
+package team.incude.gsmc.v2.domain.evidence.application.usecase.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+import team.incude.gsmc.v2.domain.evidence.application.port.ActivityEvidencePersistencePort;
+import team.incude.gsmc.v2.domain.evidence.application.port.S3Port;
+import team.incude.gsmc.v2.domain.evidence.application.usecase.CreateDraftActivityEvidenceUseCase;
+import team.incude.gsmc.v2.domain.evidence.domain.DraftActivityEvidence;
+import team.incude.gsmc.v2.domain.evidence.domain.constant.EvidenceType;
+import team.incude.gsmc.v2.domain.evidence.presentation.data.response.CreateDraftEvidenceResponse;
+import team.incude.gsmc.v2.global.thirdparty.aws.exception.S3UploadFailedException;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class CreateDraftActivityEvidenceService implements CreateDraftActivityEvidenceUseCase {
+
+    private static final Long DRAFT_TTL_SECONDS = 7 * 24 * 60 * 60L;
+
+    private final ActivityEvidencePersistencePort activityEvidencePersistencePort;
+    private final S3Port s3Port;
+
+    @Override
+    public CreateDraftEvidenceResponse execute(UUID id, String categoryName, String title, String content, MultipartFile file, String imageUrl, EvidenceType activityType) {
+        UUID draftId = id == null ? UUID.randomUUID() : id;
+
+        DraftActivityEvidence draftActivityEvidence = createActivityEvidence(draftId, categoryName, title, content, file, imageUrl, activityType);
+
+        activityEvidencePersistencePort.saveDraftActivityEvidence(draftActivityEvidence);
+        return new CreateDraftEvidenceResponse(draftId);
+    }
+
+    private DraftActivityEvidence createActivityEvidence(UUID id, String categoryName, String title, String content, MultipartFile file, String imageUrlParam, EvidenceType evidenceType) {
+        String imageUrl = null;
+
+        if (file != null && !file.isEmpty()) {
+            imageUrl = uploadFile(file);
+        } else if (imageUrlParam != null && !imageUrlParam.isBlank()) {
+            imageUrl = imageUrlParam;
+        }
+
+        return DraftActivityEvidence.builder()
+                .id(id)
+                .categoryName(categoryName)
+                .title(title)
+                .content(content)
+                .imageUrl(imageUrl)
+                .evidenceType(evidenceType)
+                .ttl(DRAFT_TTL_SECONDS)
+                .build();
+    }
+
+    private String uploadFile(MultipartFile file) {
+        try {
+            return s3Port.uploadFile(
+                    file.getOriginalFilename(),
+                    file.getInputStream()
+            ).join();
+        } catch (IOException e) {
+            throw new S3UploadFailedException();
+        }
+    }
+}
