d171a301460680c3ac2fb8b90ffddfe650accaeb
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-05-01 23:08:40 +0900

    add: 임시 저장 도메인 모델 추가

---
+++ src/main/java/team/incude/gsmc/v2/domain/evidence/domain/DraftActivityEvidence.java
@@ -0,0 +1,18 @@
+package team.incude.gsmc.v2.domain.evidence.domain;
+
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import team.incude.gsmc.v2.domain.evidence.domain.constant.EvidenceType;
+
+import java.util.UUID;
+
+@Getter
+@Entity
+public class DraftActivityEvidence {
+    private UUID id;
+    private String categoryName;
+    private String title;
+    private String content;
+    private String imageUrl;
+    private EvidenceType evidenceType;
+}
---
+++ src/main/java/team/incude/gsmc/v2/domain/evidence/domain/DraftReadingEvidence.java
@@ -0,0 +1,11 @@
+package team.incude.gsmc.v2.domain.evidence.domain;
+
+import java.util.UUID;
+
+public class DraftReadingEvidence {
+    private UUID id;
+    private String title;
+    private String author;
+    private Integer page;
+    private String content;
+}
