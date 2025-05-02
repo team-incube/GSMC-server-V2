80158cfa0393c726b35edabae197769e093c8cab
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-05-02 15:34:11 +0900

    add: 증빙자료 임시 저장 관련 persistence Port/Adapter 구현

---
+++ src/main/java/team/incude/gsmc/v2/domain/evidence/application/port/DraftReadingEvidencePersistencePort.java
@@ -0,0 +1,16 @@
+package team.incude.gsmc.v2.domain.evidence.application.port;
+
+import team.incude.gsmc.v2.domain.evidence.domain.DraftReadingEvidence;
+import team.incude.gsmc.v2.global.annotation.PortDirection;
+import team.incude.gsmc.v2.global.annotation.port.Port;
+
+import java.util.UUID;
+
+@Port(direction = PortDirection.OUTBOUND)
+public interface DraftReadingEvidencePersistencePort {
+    DraftReadingEvidence saveDraftReadingEvidence(DraftReadingEvidence draftReadingEvidence);
+
+    DraftReadingEvidence findDraftReadingEvidenceById(UUID draftId);
+
+    void deleteDraftReadingEvidenceById(UUID draftId);
+}
---
+++ src/main/java/team/incude/gsmc/v2/domain/evidence/persistence/DraftReadingEvidencePersistenceAdapter.java
@@ -0,0 +1,37 @@
+package team.incude.gsmc.v2.domain.evidence.persistence;
+
+import lombok.RequiredArgsConstructor;
+import team.incude.gsmc.v2.domain.evidence.application.port.DraftReadingEvidencePersistencePort;
+import team.incude.gsmc.v2.domain.evidence.domain.DraftReadingEvidence;
+import team.incude.gsmc.v2.domain.evidence.exception.DraftReadingEvidenceNotFoundException;
+import team.incude.gsmc.v2.domain.evidence.persistence.mapper.ReadingEvidenceMapper;
+import team.incude.gsmc.v2.domain.evidence.persistence.repository.DraftReadingEvidenceRedisRepository;
+import team.incude.gsmc.v2.global.annotation.PortDirection;
+import team.incude.gsmc.v2.global.annotation.adapter.Adapter;
+
+import java.util.UUID;
+
+@Adapter(direction = PortDirection.OUTBOUND)
+@RequiredArgsConstructor
+public class DraftReadingEvidencePersistenceAdapter implements DraftReadingEvidencePersistencePort {
+
+    private final DraftReadingEvidenceRedisRepository draftReadingEvidenceRedisRepository;
+    private final ReadingEvidenceMapper readingEvidenceMapper;
+
+    @Override
+    public DraftReadingEvidence saveDraftReadingEvidence(DraftReadingEvidence draftReadingEvidence) {
+        return readingEvidenceMapper.toDraftDomain(draftReadingEvidenceRedisRepository.save(readingEvidenceMapper.toDraftEntity(draftReadingEvidence)));
+    }
+
+    @Override
+    public DraftReadingEvidence findDraftReadingEvidenceById(UUID draftId) {
+        return draftReadingEvidenceRedisRepository.findById(draftId)
+                .map(readingEvidenceMapper::toDraftDomain)
+                .orElseThrow(DraftReadingEvidenceNotFoundException::new);
+    }
+
+    @Override
+    public void deleteDraftReadingEvidenceById(UUID draftId) {
+        draftReadingEvidenceRedisRepository.deleteById(draftId);
+    }
+}
