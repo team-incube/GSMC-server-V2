fc7d9e2dfb41a604411c0479bc975916ca6dfcae
snowykte0426 <snowykte0426@naver.com>
2025-05-05 00:15:33 +0900

    docs: 사용자 관련 모델 서비스 클래스 주석추가

--- src/main/java/team/incude/gsmc/v2/domain/member/application/usecase/service/SearchStudentService.java
+++ src/main/java/team/incude/gsmc/v2/domain/member/application/usecase/service/SearchStudentService.java
@@ -10,6 +10,14 @@ import team.incude.gsmc.v2.domain.member.domain.StudentDetailWithEvidence;
 import team.incude.gsmc.v2.domain.member.presentation.data.response.GetStudentResponse;
 import team.incude.gsmc.v2.domain.member.presentation.data.response.SearchStudentResponse;
 
+/**
+ * 학생 조건 검색 유스케이스의 구현 클래스입니다.
+ * <p>{@link SearchStudentUseCase}를 구현하며, 이름, 학년, 반 번호를 기준으로 필터링된 학생 목록을 페이징 처리하여 조회합니다.
+ * <p>{@link StudentDetailPersistencePort}를 통해 영속성 계층에서 검색 쿼리를 수행하고,
+ * 결과는 {@link SearchStudentResponse}로 매핑되어 클라이언트에 전달됩니다.
+ * <p>검색 결과에는 학생의 점수, 증빙자료 상태, 권한 정보 등이 포함됩니다.
+ * @author snowykte0426
+ */
 @Service
 @RequiredArgsConstructor
 public class SearchStudentService implements SearchStudentUseCase {
