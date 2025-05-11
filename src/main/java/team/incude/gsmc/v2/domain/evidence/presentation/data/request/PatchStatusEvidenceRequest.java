da16378994b9eab9bbb15e4eb49fd8229a0fd294
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-05-11 15:10:21 +0900

    update: 증빙자료 상태 변경 request dto에 @NotNull 어노테이션 추가

--- src/main/java/team/incude/gsmc/v2/domain/evidence/presentation/data/request/PatchStatusEvidenceRequest.java
+++ src/main/java/team/incude/gsmc/v2/domain/evidence/presentation/data/request/PatchStatusEvidenceRequest.java
@@ -1,8 +1,9 @@
 package team.incude.gsmc.v2.domain.evidence.presentation.data.request;
 
+import jakarta.validation.constraints.NotNull;
 import team.incude.gsmc.v2.domain.evidence.domain.constant.ReviewStatus;
 
 public record PatchStatusEvidenceRequest(
-        ReviewStatus status
+        @NotNull ReviewStatus status
 ) {
 }
