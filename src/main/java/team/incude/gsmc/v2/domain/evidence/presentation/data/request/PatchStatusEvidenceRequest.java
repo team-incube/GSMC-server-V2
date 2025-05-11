2d585909e3e5a899fc2d1049451ab6e3d822a227
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-05-11 15:15:49 +0900

    Revert "update: 증빙자료 상태 변경 request dto에 @NotNull 어노테이션 추가"
    This reverts commit da16378994b9eab9bbb15e4eb49fd8229a0fd294.

--- src/main/java/team/incude/gsmc/v2/domain/evidence/presentation/data/request/PatchStatusEvidenceRequest.java
+++ src/main/java/team/incude/gsmc/v2/domain/evidence/presentation/data/request/PatchStatusEvidenceRequest.java
@@ -1,9 +1,8 @@
 package team.incude.gsmc.v2.domain.evidence.presentation.data.request;
 
-import jakarta.validation.constraints.NotNull;
 import team.incude.gsmc.v2.domain.evidence.domain.constant.ReviewStatus;
 
 public record PatchStatusEvidenceRequest(
-        @NotNull ReviewStatus status
+        ReviewStatus status
 ) {
 }
