2a395300b9793b86824e9696d6f1ca7a29f95f00
snowykte0426 <snowykte0426@naver.com>
2025-05-14 00:14:47 +0900

    test: Spring Test 통합 테스트 사용 프로파일 강제화

--- src/test/java/team/incude/gsmc/v2/GsmcApplicationTests.java
+++ src/test/java/team/incude/gsmc/v2/GsmcApplicationTests.java
@@ -2,8 +2,10 @@ package team.incude.gsmc.v2;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class GsmcApplicationTests {
 
 	@Test
