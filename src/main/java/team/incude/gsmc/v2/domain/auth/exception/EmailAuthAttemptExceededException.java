d92d0ed862cb4b7c1bb55eb38946b4fb0096e8a8
snowykte0426 <snowykte0426@naver.com>
2025-05-14 22:37:51 +0900

    docs: 인증/인가 도메인 모델 예외 클래스 JavaDoc 주석추가

--- src/main/java/team/incude/gsmc/v2/domain/auth/exception/EmailAuthAttemptExceededException.java
+++ src/main/java/team/incude/gsmc/v2/domain/auth/exception/EmailAuthAttemptExceededException.java
@@ -3,6 +3,16 @@ package team.incude.gsmc.v2.domain.auth.exception;
 import team.incude.gsmc.v2.global.error.ErrorCode;
 import team.incude.gsmc.v2.global.error.exception.GsmcException;
 
+
+/**
+ * 이메일 인증 시도 횟수를 초과했을 때 발생하는 예외입니다.
+ * <p>Redis에 저장된 인증 요청 시도 횟수가 설정된 제한을 초과한 경우 발생합니다.
+ * 보안 상의 이유로 일정 횟수 이상 인증을 시도한 사용자의 이메일 인증을 제한합니다.
+ * <p>{@link team.incude.gsmc.v2.global.error.ErrorCode#EMAIL_AUTH_ATTEMPT_EXCEEDED}에 매핑되어
+ * 클라이언트에 적절한 오류 메시지와 상태 코드를 전달합니다.
+ * @see team.incude.gsmc.v2.domain.auth.application.usecase.service.SendAuthenticationEmailService
+ * @author jihoonwjj
+ */
 public class EmailAuthAttemptExceededException extends GsmcException {
     public EmailAuthAttemptExceededException() {
         super(ErrorCode.EMAIL_AUTH_ATTEMPT_EXCEEDED);
