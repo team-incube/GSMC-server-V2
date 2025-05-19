240e2853cb5902d6ac38e08001c3712000279521
황지훈 <s24072@gsm.hs.kr>
2025-05-20 00:54:00 +0900

    add: JwtPersistencePort 구현

---
+++ src/main/java/team/incude/gsmc/v2/global/security/jwt/application/port/JwtPersistencePort.java
@@ -0,0 +1,23 @@
+package team.incude.gsmc.v2.global.security.jwt.application.port;
+
+import jakarta.servlet.http.HttpServletRequest;
+import team.incude.gsmc.v2.domain.member.domain.constant.MemberRole;
+import team.incude.gsmc.v2.global.security.jwt.presentation.dto.TokenDto;
+
+public interface JwtPersistencePort {
+    TokenDto issueAccessToken(String email, MemberRole roles);
+
+    TokenDto issueRefreshToken(String email);
+
+    Boolean validateAccessToken(String accessToken);
+
+    Boolean validateRefreshToken(String refreshToken);
+
+    String getEmailFromAccessToken(String accessToken);
+
+    String getEmailFromRefreshToken(String refreshToken);
+
+    MemberRole getRolesFromAccessToken(String accessToken);
+
+    String resolveToken(HttpServletRequest request);
+}
