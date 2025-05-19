1c9c722c55f3686e421311dc5c49c07dd4ad75ec
황지훈 <s24072@gsm.hs.kr>
2025-05-20 00:53:50 +0900

    add: JwtPersistenceAdapter 구현

---
+++ src/main/java/team/incude/gsmc/v2/global/security/jwt/persistence/JwtPersistenceAdapter.java
@@ -0,0 +1,53 @@
+package team.incude.gsmc.v2.global.security.jwt.persistence;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import team.incude.gsmc.v2.domain.member.domain.constant.MemberRole;
+import team.incude.gsmc.v2.global.annotation.PortDirection;
+import team.incude.gsmc.v2.global.annotation.adapter.Adapter;
+import team.incude.gsmc.v2.global.security.jwt.application.port.JwtPersistencePort;
+import team.incude.gsmc.v2.global.security.jwt.application.usecase.JwtIssueUseCase;
+import team.incude.gsmc.v2.global.security.jwt.application.usecase.JwtParserUseCase;
+import team.incude.gsmc.v2.global.security.jwt.application.usecase.JwtRefreshManagementUseCase;
+import team.incude.gsmc.v2.global.security.jwt.presentation.dto.TokenDto;
+
+@Adapter(direction = PortDirection.INBOUND)
+@RequiredArgsConstructor
+public class JwtPersistenceAdapter implements JwtPersistencePort {
+
+    private final JwtIssueUseCase jwtIssueUseCase;
+    private final JwtParserUseCase jwtParserUseCase;
+    private final JwtRefreshManagementUseCase jwtRefreshManagementUseCase;
+
+    public TokenDto issueAccessToken(String email, MemberRole roles) {
+        return jwtIssueUseCase.issueAccessToken(email, roles);
+    }
+
+    public TokenDto issueRefreshToken(String email) {
+        return jwtIssueUseCase.issueRefreshToken(email);
+    }
+
+    public Boolean validateAccessToken(String token) {
+        return jwtParserUseCase.validateAccessToken(token);
+    }
+
+    public Boolean validateRefreshToken(String token) {
+        return jwtParserUseCase.validateRefreshToken(token);
+    }
+
+    public String getEmailFromAccessToken(String token) {
+        return jwtParserUseCase.getEmailFromAccessToken(token);
+    }
+
+    public String getEmailFromRefreshToken(String token) {
+        return jwtParserUseCase.getEmailFromRefreshToken(token);
+    }
+
+    public MemberRole getRolesFromAccessToken(String token) {
+        return jwtParserUseCase.getRolesFromAccessToken(token);
+    }
+
+    public String resolveToken(HttpServletRequest request) {
+        return jwtParserUseCase.resolveToken(request);
+    }
+}
