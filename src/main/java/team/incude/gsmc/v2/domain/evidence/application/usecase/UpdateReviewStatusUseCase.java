7a863dae86cec41b50914c4914082b14b0b1703e
snowykte0426 <snowykte0426@naver.com>
2025-05-20 16:37:11 +0900

    docs: 증빙자료 모델 Use Case 인터페이스 JavaDoc 주석추가

--- src/main/java/team/incude/gsmc/v2/domain/evidence/application/usecase/UpdateReviewStatusUseCase.java
+++ src/main/java/team/incude/gsmc/v2/domain/evidence/application/usecase/UpdateReviewStatusUseCase.java
@@ -2,6 +2,12 @@ package team.incude.gsmc.v2.domain.evidence.application.usecase;
 
 import team.incude.gsmc.v2.domain.evidence.domain.constant.ReviewStatus;
 
+/**
+ * 증빙자료의 검토 상태를 변경하는 유스케이스 인터페이스입니다.
+ * <p>관리자 또는 검토 권한이 있는 사용자가 해당 증빙자료의 검토 상태를
+ * {@link team.incude.gsmc.v2.domain.evidence.domain.constant.ReviewStatus} 값으로 갱신할 수 있도록 정의합니다.
+ * @author suuuuuuminnnnnn
+ */
 public interface UpdateReviewStatusUseCase {
     void execute(Long evidenceId, ReviewStatus reviewStatus);
 }
