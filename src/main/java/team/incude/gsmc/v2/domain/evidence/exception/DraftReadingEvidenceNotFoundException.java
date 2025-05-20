cbb1836a2a4aa310d241b57f6c130291af8924b9
snowykte0426 <snowykte0426@naver.com>
2025-05-20 19:45:35 +0900

    docs: 증빙자료 모델 예외 클래스 JavaDoc 주석추가

--- src/main/java/team/incude/gsmc/v2/domain/evidence/exception/DraftReadingEvidenceNotFoundException.java
+++ src/main/java/team/incude/gsmc/v2/domain/evidence/exception/DraftReadingEvidenceNotFoundException.java
@@ -3,6 +3,12 @@ package team.incude.gsmc.v2.domain.evidence.exception;
 import team.incude.gsmc.v2.global.error.ErrorCode;
 import team.incude.gsmc.v2.global.error.exception.GsmcException;
 
+/**
+ * 독서 증빙자료 임시저장이 존재하지 않을 때 발생하는 예외입니다.
+ * <p>예: 주어진 UUID로 임시저장을 조회했지만 존재하지 않는 경우
+ * <p>예외 발생 시 {@link team.incude.gsmc.v2.global.error.ErrorCode#DRAFT_READING_EVIDENCE_NOT_FOUND} 코드가 반환됩니다.
+ * @author suuuuuuminnnnnn
+ */
 public class DraftReadingEvidenceNotFoundException extends GsmcException {
     public DraftReadingEvidenceNotFoundException() {
         super(ErrorCode.DRAFT_READING_EVIDENCE_NOT_FOUND);
