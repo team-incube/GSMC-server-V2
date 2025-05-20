070d1ba269e5d8200ba2f3cb56c805fe2dca43ea
황지훈 <s24072@gsm.hs.kr>
2025-05-20 15:23:30 +0900

    add: JwtPort javadocs 추가

--- src/main/java/team/incude/gsmc/v2/domain/auth/application/port/JwtPort.java
+++ src/main/java/team/incude/gsmc/v2/domain/auth/application/port/JwtPort.java
@@ -4,6 +4,22 @@ import jakarta.servlet.http.HttpServletRequest;
 import team.incude.gsmc.v2.domain.member.domain.constant.MemberRole;
 import team.incude.gsmc.v2.global.security.jwt.dto.TokenDto;
 
+/**
+ * Jwt 관련 유스케이스를 정의하는 포트 인터페이스입니다.
+ * <p> 토큰발급, 유효성검사, 이메일추출 등 토큰 관련 기능을 제공합니다.
+ * 주요기능:
+ * <ul>
+ *    <li>{@code issueAccessToken} - 새로운 액세스토큰 발급 </li>
+ *    <li>{@code issueRefreshToken} - 새로운 리프레시토큰 발급 </li>
+ *    <li>{@code validateAccessToken} - 액세스토큰의 유효성 검사 </li>
+ *    <li>{@code validateRefreshToken} - 리프레시토클의 유효성 검사 </li>
+ *    <li>{@code getEmailFromAccessToken} - 액세스토큰 이메일 추출 </li>
+ *    <li>{@code getEmailFromRefreshToken} - 리프레시토큰 이메일 추출 </li>
+ *    <li>{@code getRolesFromAccessToken} - 액세스토큰에서 역할 추출 </li>
+ *    <li>{@code resolveToken} - 토큰 파싱 </li>
+ * </ul>
+ * @author jihoonwjj
+ */
 public interface JwtPort {
     TokenDto issueAccessToken(String email, MemberRole roles);
 
