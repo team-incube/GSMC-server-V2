11d39daee7e85d12b4fb87c86c916517072433e1
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-05-30 22:42:17 +0900

    docs: 문서화 수정

--- src/main/java/team/incude/gsmc/v2/global/event/handler/DraftEvidenceDeleteEventListener.java
+++ src/main/java/team/incude/gsmc/v2/global/event/handler/DraftEvidenceDeleteEventListener.java
@@ -8,6 +8,12 @@ import team.incude.gsmc.v2.domain.evidence.application.port.DraftActivityEvidenc
 import team.incude.gsmc.v2.domain.evidence.application.port.DraftReadingEvidencePersistencePort;
 import team.incude.gsmc.v2.global.event.DraftEvidenceDeleteEvent;
 
+/**
+ * 임시 저장 증빙 삭제 이벤트를 처리하는 이벤트 리스너 클래스입니다.
+ * <p>{@link DraftEvidenceDeleteEvent} 이벤트가 발생하면 활동 및 독서 증빙 초안을 삭제합니다.
+ * 해당 동작은 비동기적으로 수행되며, 임시 저장 ID가 null인 경우는 무시됩니다.
+ * @author suuuuuuminnnnnn
+ */
 @Component
 @RequiredArgsConstructor
 public class DraftEvidenceDeleteEventListener {
