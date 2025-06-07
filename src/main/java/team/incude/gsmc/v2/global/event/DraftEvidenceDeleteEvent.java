b18085eef5b59b5b2a4db113944dde17b9420866
snowykte0426 <snowykte0426@naver.com>
2025-06-07 21:47:11 +0900

    docs: 임시저장 증빙자료 삭제 이벤트 클래스 JavaDoc 주석 저자 태그 추가작성

--- src/main/java/team/incude/gsmc/v2/global/event/DraftEvidenceDeleteEvent.java
+++ src/main/java/team/incude/gsmc/v2/global/event/DraftEvidenceDeleteEvent.java
@@ -7,6 +7,7 @@ import java.util.UUID;
  * <p>이 이벤트는 특정 임시 저장 ID에 대한 증빙 삭제 작업을 수행하기 위해 사용됩니다.
  * {@code draftId}를 통해 삭제할 임시 저장 증빙을 식별합니다.
  * @param draftId 삭제할 임시 저장 증빙의 ID
+ * @author suuuuuuminnnnnn
  */
 public record DraftEvidenceDeleteEvent(UUID draftId) {
 }
