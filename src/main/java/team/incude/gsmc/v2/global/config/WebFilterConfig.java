61e07bbd815f7056f48d507cd6d035d23150b0a1
snowykte0426 <snowykte0426@naver.com>
2025-06-10 12:01:49 +0900

    docs: Filter 설정 클래스 JavaDoc 주석 추가

--- src/main/java/team/incude/gsmc/v2/global/config/WebFilterConfig.java
+++ src/main/java/team/incude/gsmc/v2/global/config/WebFilterConfig.java
@@ -5,6 +5,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import team.incude.gsmc.v2.global.filter.logging.HttpLoggingFilter;
 
+/**
+ * HTTP 요청 및 응답 로깅을 위한 필터 설정 클래스입니다.
+ * <p>이 클래스는 {@link HttpLoggingFilter}를 등록하여 모든 HTTP 요청과 응답을 로깅합니다.
+ * <p>필터는 최우선 순위로 설정되어, 다른 필터보다 먼저 실행됩니다.
+ * <p>모든 URL 패턴에 대해 적용되며, 개발 및 디버깅 목적으로 유용합니다.
+ * @author jihoonwjj
+ */
 @Configuration
 public class WebFilterConfig {
 
