4548032841d748e7a88c35df5cd2b83120837d60
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-06-14 23:07:55 +0900

    update: 증빙자료 타입 필드 추가

--- src/main/java/team/incude/gsmc/v2/global/event/FileUploadEvent.java
+++ src/main/java/team/incude/gsmc/v2/global/event/FileUploadEvent.java
@@ -1,10 +1,13 @@
 package team.incude.gsmc.v2.global.event;
 
+import team.incude.gsmc.v2.domain.evidence.domain.constant.EvidenceType;
+
 import java.io.InputStream;
 
 public record FileUploadEvent(
         Long evidenceId,
         String fileName,
-        InputStream inputStream
+        InputStream inputStream,
+        EvidenceType evidenceType
 ) {
 }
