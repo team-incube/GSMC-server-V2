2fef063bfcb4825ce82578d1012a3c684e7e4a8b
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-06-17 20:43:48 +0900

    add: s3 업로드 이벤트 리스너 추가

---
+++ src/main/java/team/incude/gsmc/v2/global/event/handler/FileUploadEventListener.java
@@ -0,0 +1,26 @@
+package team.incude.gsmc.v2.global.event.handler;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.event.TransactionalEventListener;
+import team.incude.gsmc.v2.domain.evidence.application.port.EvidenceApplicationPort;
+import team.incude.gsmc.v2.global.event.FileUploadEvent;
+
+import static org.springframework.transaction.event.TransactionPhase.AFTER_COMMIT;
+
+@Component
+@RequiredArgsConstructor
+public class FileUploadEventListener {
+
+    private final EvidenceApplicationPort evidenceApplicationPort;
+
+    @Async
+    @TransactionalEventListener(phase = AFTER_COMMIT)
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    public void handleFileUploadEvent(FileUploadEvent event) {
+        evidenceApplicationPort.updateEvidenceFile(event.evidenceId(), event.fileName(), event.inputStream(), event.evidenceType());
+    }
+}
