1da40e2c8a06b7f9fc4e046c5cd878320082f418
황지훈 <s24072@gsm.hs.kr>
2025-06-19 17:16:53 +0900

    update: DomainAuthorizationConfig 개행 추가

--- src/main/java/team/incude/gsmc/v2/global/security/config/DomainAuthorizationConfig.java
+++ src/main/java/team/incude/gsmc/v2/global/security/config/DomainAuthorizationConfig.java
@@ -12,35 +12,103 @@ public class DomainAuthorizationConfig {
     public void configureAuthorization(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(auth -> auth
                 .requestMatchers(HttpMethod.GET, 
-                "/api/v2/members/students")
-                .hasAnyAuthority(MemberRole.ROLE_ADMIN.name())
-                .requestMatchers(HttpMethod.GET, "/api/v2/members/students/search").hasAnyAuthority(MemberRole.ROLE_ADMIN.name())
-                .requestMatchers(HttpMethod.GET, "/api/v2/members/students/current").hasAnyAuthority(MemberRole.ROLE_STUDENT.name())
-                .requestMatchers(HttpMethod.GET, "/api/v2/certificates/current").hasAnyAuthority(MemberRole.ROLE_STUDENT.name())
-                .requestMatchers(HttpMethod.GET, "/api/v2/certificates/{email}").hasAnyAuthority(MemberRole.ROLE_ADMIN.name())
-                .requestMatchers(HttpMethod.DELETE, "/api/v2/certificates/current/{certificateId}").hasAnyAuthority(MemberRole.ROLE_STUDENT.name())
-                .requestMatchers(HttpMethod.DELETE, "/api/v2/certificates/{email}/{certificateId}").hasAnyAuthority(MemberRole.ROLE_ADMIN.name())
-                .requestMatchers(HttpMethod.PATCH, "/api/v2/certificates/current/{certificateId}").hasAnyAuthority(MemberRole.ROLE_STUDENT.name())
-                .requestMatchers(HttpMethod.POST, "/api/v2/certificates").hasAnyAuthority(MemberRole.ROLE_STUDENT.name())
-                .requestMatchers(HttpMethod.GET, "/api/v2/score/current").hasAnyAuthority(MemberRole.ROLE_STUDENT.name())
-                .requestMatchers(HttpMethod.GET, "/api/v2/score/{studentCode}").hasAnyAuthority(MemberRole.ROLE_ADMIN.name())
-                .requestMatchers(HttpMethod.PATCH, "/api/v2/score/current").hasAnyAuthority(MemberRole.ROLE_STUDENT.name())
-                .requestMatchers(HttpMethod.PATCH, "/api/v2/score/{studentCode}").hasAnyAuthority(MemberRole.ROLE_ADMIN.name())
-                .requestMatchers(HttpMethod.GET, "/api/v2/evidence/current").hasAnyAuthority(MemberRole.ROLE_STUDENT.name())
-                .requestMatchers(HttpMethod.GET, "/api/v2/evidence/{studentCode}").hasAnyAuthority(MemberRole.ROLE_ADMIN.name())
-                .requestMatchers(HttpMethod.GET, "/api/v2/evidence/search").hasAnyAuthority(MemberRole.ROLE_STUDENT.name())
-                .requestMatchers(HttpMethod.POST, "/api/v2/evidence/current/activity").hasAnyAuthority(MemberRole.ROLE_STUDENT.name())
-                .requestMatchers(HttpMethod.POST, "/api/v2/evidence/current/reading").hasAnyAuthority(MemberRole.ROLE_STUDENT.name())
-                .requestMatchers(HttpMethod.POST, "/api/v2/evidence/current/other").hasAnyAuthority(MemberRole.ROLE_STUDENT.name())
-                .requestMatchers(HttpMethod.POST, "/api/v2/evidence/current/scoring").hasAnyAuthority(MemberRole.ROLE_STUDENT.name())
-                .requestMatchers(HttpMethod.PATCH, "/api/v2/evidence/{evidenceId}/status").hasAnyAuthority(MemberRole.ROLE_ADMIN.name())
-                .requestMatchers(HttpMethod.DELETE, "/api/v2/evidence/current/{evidenceId}").hasAnyAuthority(MemberRole.ROLE_STUDENT.name())
-                .requestMatchers(HttpMethod.POST, "/api/v2/evidence/current/draft/activity").hasAnyAuthority(MemberRole.ROLE_STUDENT.name())
-                .requestMatchers(HttpMethod.POST, "/api/v2/evidence/current/draft/reading").hasAnyAuthority(MemberRole.ROLE_STUDENT.name())
-                .requestMatchers(HttpMethod.GET, "/api/v2/evidence/draft/activity/{draftId}").hasAnyAuthority(MemberRole.ROLE_STUDENT.name())
-                .requestMatchers(HttpMethod.GET, "/api/v2/evidence/draft/reading/{draftId}").hasAnyAuthority(MemberRole.ROLE_STUDENT.name())
-                .requestMatchers(HttpMethod.GET, "/api/v2/evidence/current/draft").hasAnyAuthority(MemberRole.ROLE_STUDENT.name())
-                .requestMatchers(HttpMethod.GET, "/api/v2/sheet/{grade}/{classNumber}").hasAnyAuthority(MemberRole.ROLE_ADMIN.name())
+                    "api/v2/members/students",
+                    "api/v2/members/students/search"
+                )
+                .hasAnyAuthority(
+                    MemberRole.ROLE_ADMIN.name()
+                )
+
+                .requestMatchers(HttpMethod.GET, "/api/v2/members/students/current").hasAnyAuthority(
+                    MemberRole.ROLE_STUDENT.name()
+                )
+
+                .requestMatchers(HttpMethod.GET, "/api/v2/certificates/current").hasAnyAuthority(
+                    MemberRole.ROLE_STUDENT.name()
+                )
+
+                .requestMatchers(HttpMethod.GET, "/api/v2/certificates/{email}").hasAnyAuthority(
+                    MemberRole.ROLE_ADMIN.name()
+                )
+
+                .requestMatchers(HttpMethod.DELETE, "/api/v2/certificates/current/{certificateId}").hasAnyAuthority(
+                    MemberRole.ROLE_STUDENT.name()
+                )
+
+                .requestMatchers(HttpMethod.DELETE, "/api/v2/certificates/{email}/{certificateId}").hasAnyAuthority(
+                    MemberRole.ROLE_ADMIN.name()
+                )
+
+                .requestMatchers(HttpMethod.PATCH, "/api/v2/certificates/current/{certificateId}").hasAnyAuthority(
+                    MemberRole.ROLE_STUDENT.name()
+                )
+
+                .requestMatchers(HttpMethod.GET, "/api/v2/score/current").hasAnyAuthority(
+                    MemberRole.ROLE_STUDENT.name()
+                )
+
+                .requestMatchers(HttpMethod.GET, "/api/v2/score/{studentCode}").hasAnyAuthority(
+                    MemberRole.ROLE_ADMIN.name()
+                )
+
+                .requestMatchers(HttpMethod.PATCH, "/api/v2/score/current").hasAnyAuthority(
+                    MemberRole.ROLE_STUDENT.name()
+                )
+
+                .requestMatchers(HttpMethod.PATCH, "/api/v2/score/{studentCode}").hasAnyAuthority(
+                    MemberRole.ROLE_ADMIN.name()
+                )
+
+                .requestMatchers(HttpMethod.GET, 
+                    "/api/v2/evidence/current",
+                    "/api/v2/evidence/search"
+                )
+                .hasAnyAuthority(
+                    MemberRole.ROLE_STUDENT.name()
+                )
+
+                .requestMatchers(HttpMethod.GET, "/api/v2/evidence/{studentCode}").hasAnyAuthority(
+                    MemberRole.ROLE_ADMIN.name()
+                )
+
+                .requestMatchers(HttpMethod.POST, 
+                    "/api/v2/evidence/current/activity",
+                    "/api/v2/evidence/current/reading",
+                    "/api/v2/evidence/current/other",
+                    "/api/v2/evidence/current/scoring"
+                )
+                .hasAnyAuthority(
+                    MemberRole.ROLE_STUDENT.name()
+                )
+
+                .requestMatchers(HttpMethod.PATCH, "/api/v2/evidence/{evidenceId}/status").hasAnyAuthority(
+                    MemberRole.ROLE_ADMIN.name()
+                )
+
+                .requestMatchers(HttpMethod.DELETE, "/api/v2/evidence/current/{evidenceId}").hasAnyAuthority(
+                    MemberRole.ROLE_STUDENT.name()
+                )
+
+                .requestMatchers(HttpMethod.POST, 
+                    "api/v2/evidence/current/draft/activity",
+                    "/api/v2/evidence/current/draft/reading"
+                )
+                .hasAnyAuthority(
+                    MemberRole.ROLE_STUDENT.name()
+                )
+                .requestMatchers(HttpMethod.GET,
+                    "/api/v2/evidence/draft/activity/{draftId}",
+                    "/api/v2/evidence/draft/reading/{draftId}",
+                    "/api/v2/evidence/current/draft"
+                )
+                .hasAnyAuthority(
+                    MemberRole.ROLE_STUDENT.name()
+                )
+
+                .requestMatchers(HttpMethod.GET, "/api/v2/sheet/{grade}/{classNumber}").hasAnyAuthority(
+                    MemberRole.ROLE_ADMIN.name()
+                )
+
                 .anyRequest().permitAll()
         );
     }
