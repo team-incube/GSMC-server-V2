d517bbf6b6536115a01ed7c055eb61938854b541
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-06-20 07:56:30 +0900

    test: 변경된 서비스 클래스 로직에 따라 테스트 코드 변경

--- src/test/java/team/incude/gsmc/v2/domain/evidence/application/usecase/service/CreateOtherEvidenceServiceTest.java
+++ src/test/java/team/incude/gsmc/v2/domain/evidence/application/usecase/service/CreateOtherEvidenceServiceTest.java
@@ -6,24 +6,28 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.web.multipart.MultipartFile;
+import team.incude.gsmc.v2.domain.evidence.application.port.EvidencePersistencePort;
 import team.incude.gsmc.v2.domain.evidence.application.port.OtherEvidencePersistencePort;
-import team.incude.gsmc.v2.domain.evidence.application.port.S3Port;
+import team.incude.gsmc.v2.domain.evidence.domain.Evidence;
 import team.incude.gsmc.v2.domain.evidence.domain.OtherEvidence;
 import team.incude.gsmc.v2.domain.member.domain.Member;
+import team.incude.gsmc.v2.domain.score.application.port.CategoryPersistencePort;
 import team.incude.gsmc.v2.domain.score.application.port.ScorePersistencePort;
 import team.incude.gsmc.v2.domain.score.domain.Category;
 import team.incude.gsmc.v2.domain.score.domain.Score;
+import team.incude.gsmc.v2.global.event.FileUploadEvent;
 import team.incude.gsmc.v2.global.event.ScoreUpdatedEvent;
 import team.incude.gsmc.v2.global.security.jwt.application.usecase.service.CurrentMemberProvider;
 
 import java.io.IOException;
-import java.util.concurrent.CompletableFuture;
+import java.util.List;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.argThat;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -31,13 +35,13 @@ import static org.mockito.Mockito.*;
 public class CreateOtherEvidenceServiceTest {
 
     @Mock
-    private S3Port s3Port;
-
-    @Mock
     private OtherEvidencePersistencePort otherEvidencePersistencePort;
 
     @Mock
     private ScorePersistencePort scorePersistencePort;
+
+    @Mock
+    private EvidencePersistencePort evidencePersistencePort;
 
     @Mock
     private CurrentMemberProvider currentMemberProvider;
@@ -57,18 +61,18 @@ public class CreateOtherEvidenceServiceTest {
         class Context_with_valid_request {
 
             @Test
-            @DisplayName("기타 증빙자료를 등록하고 점수를 저장한다")
-            void it_creates_other_evidence_and_updates_score () throws IOException {
+            @DisplayName("기타 증빙자료를 생성하고 점수를 갱신한다")
+            void it_creates_other_evidence_and_updates_score() throws IOException {
                 // given
-                String categoryName = "MAJOR-AWARD_CAREER-OUT_SCHOOL-OFFICIAL";
-                String fakeFileUrl = "https://s3.url/evidence.png";
+                String categoryName = "HUMANITIES-READING-READ_A_THON-TURTLE";
 
                 MultipartFile file = new MockMultipartFile(
                         "file",
                         "evidence.png",
-                        "application/png",
+                        "image/png",
                         "test".getBytes()
                 );
+
                 Member member = Member.builder()
                         .email("s24035@gsm.hs.kr")
                         .build();
@@ -84,18 +88,20 @@ public class CreateOtherEvidenceServiceTest {
                         .member(member)
                         .build();
 
+                // mocking
                 when(currentMemberProvider.getCurrentUser()).thenReturn(member);
-                when(scorePersistencePort.findScoreByCategoryNameAndMemberEmailWithLock(category.getName(), member.getEmail())).thenReturn(score);
-                when(s3Port.uploadFile(Mockito.anyString(), Mockito.any()))
-                        .thenReturn(CompletableFuture.completedFuture(fakeFileUrl));
+                when(scorePersistencePort.findScoreByCategoryNameAndMemberEmailWithLock(categoryName, member.getEmail()))
+                        .thenReturn(score);
+                when(scorePersistencePort.saveScore(any())).thenAnswer(invocation -> invocation.getArgument(0));
+                when(evidencePersistencePort.saveEvidence(any())).thenAnswer(invocation -> invocation.getArgument(0));
 
                 // when
                 createOtherEvidenceService.execute(categoryName, file);
 
                 // then
-                verify(scorePersistencePort).saveScore(Mockito.any(Score.class));
                 verify(scorePersistencePort).saveScore(argThat(savedScore -> savedScore.getValue() == 3));
-                verify(otherEvidencePersistencePort).saveOtherEvidence(any(), Mockito.any(OtherEvidence.class));
+                verify(evidencePersistencePort).saveEvidence(any(Evidence.class));
+                verify(otherEvidencePersistencePort).saveOtherEvidence(any(), any(OtherEvidence.class));
                 verify(applicationEventPublisher).publishEvent(argThat((Object event) ->
                         event instanceof ScoreUpdatedEvent &&
                                 ((ScoreUpdatedEvent) event).email().equals(member.getEmail())
