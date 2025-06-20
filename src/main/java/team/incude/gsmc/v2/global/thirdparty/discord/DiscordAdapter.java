273ae11c07ce93df1e6ca032408598db4c66363e
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-06-20 10:55:34 +0900

    update: SendEvidenceUploadFailureAlertUseCase로 의존성 주입 변경

--- src/main/java/team/incude/gsmc/v2/global/thirdparty/discord/DiscordAdapter.java
+++ src/main/java/team/incude/gsmc/v2/global/thirdparty/discord/DiscordAdapter.java
@@ -4,16 +4,17 @@ import lombok.RequiredArgsConstructor;
 import team.incude.gsmc.v2.domain.evidence.application.port.DiscordPort;
 import team.incude.gsmc.v2.global.annotation.PortDirection;
 import team.incude.gsmc.v2.global.annotation.adapter.Adapter;
-import team.incude.gsmc.v2.global.thirdparty.discord.service.SendEvidenceUploadFailureAlertService;
+import team.incude.gsmc.v2.global.thirdparty.discord.service.SendEvidenceUploadFailureAlertUseCase;
+import team.incude.gsmc.v2.global.thirdparty.discord.service.impl.SendEvidenceUploadFailureAlertService;
 
 @Adapter(direction = PortDirection.OUTBOUND)
 @RequiredArgsConstructor
 public class DiscordAdapter implements DiscordPort {
 
-    private final SendEvidenceUploadFailureAlertService fileUploadDiscordAlertService;
+    private final SendEvidenceUploadFailureAlertUseCase sendEvidenceUploadFailureAlertUseCase;
 
     @Override
     public void sendEvidenceUploadFailureAlert(Long evidenceId, String fileName, String email, Throwable exception) {
-        fileUploadDiscordAlertService.sendEvidenceUploadFailureAlert(evidenceId, fileName, email, exception);
+        sendEvidenceUploadFailureAlertUseCase.sendEvidenceUploadFailureAlert(evidenceId, fileName, email, exception);
     }
 }
