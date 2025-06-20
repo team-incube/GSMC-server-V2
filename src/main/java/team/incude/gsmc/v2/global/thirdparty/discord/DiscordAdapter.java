0635d7b4776f9293891336cd764f8fd6c0aa33c3
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-06-20 10:24:10 +0900

    add: discord port/adapter 추가

---
+++ src/main/java/team/incude/gsmc/v2/global/thirdparty/discord/DiscordAdapter.java
@@ -0,0 +1,19 @@
+package team.incude.gsmc.v2.global.thirdparty.discord;
+
+import lombok.RequiredArgsConstructor;
+import team.incude.gsmc.v2.domain.evidence.application.port.DiscordPort;
+import team.incude.gsmc.v2.global.annotation.PortDirection;
+import team.incude.gsmc.v2.global.annotation.adapter.Adapter;
+import team.incude.gsmc.v2.global.thirdparty.discord.service.FIleUploadDiscordAlertService;
+
+@Adapter(direction = PortDirection.OUTBOUND)
+@RequiredArgsConstructor
+public class DiscordAdapter implements DiscordPort {
+
+    private final FIleUploadDiscordAlertService fileUploadDiscordAlertService;
+
+    @Override
+    public void sendEvidenceUploadFailureAlert(Long evidenceId, String fileName, String email, Throwable exception) {
+        fileUploadDiscordAlertService.sendEvidenceUploadFailureAlert(evidenceId, fileName, email, exception);
+    }
+}
