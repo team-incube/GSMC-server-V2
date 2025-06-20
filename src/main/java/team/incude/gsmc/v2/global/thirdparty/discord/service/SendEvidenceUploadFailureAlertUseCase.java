1407c2078304231620a0f5a042ee535608a50085
snowykte0426 <snowykte0426@naver.com>
2025-06-20 11:25:23 +0900

    docs: 새로 추가된 Discord 관련 경고 서비스클래스 JavaDoc 주석 추가

--- src/main/java/team/incude/gsmc/v2/global/thirdparty/discord/service/SendEvidenceUploadFailureAlertUseCase.java
+++
@@ -1,5 +0,0 @@
-package team.incude.gsmc.v2.global.thirdparty.discord.service;
-
-public interface SendEvidenceUploadFailureAlertUseCase {
-    void sendEvidenceUploadFailureAlert(Long evidenceId, String fileName, String email, Throwable exception);
-}
