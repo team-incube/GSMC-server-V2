fd6c653e28b25ae855923153dc86b2242a2e9048
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-06-23 09:01:51 +0900

    add: 예외처리 try-catch 문 추가

--- src/main/java/team/incude/gsmc/v2/domain/evidence/application/usecase/service/UpdateEvidenceFileService.java
+++ src/main/java/team/incude/gsmc/v2/domain/evidence/application/usecase/service/UpdateEvidenceFileService.java
@@ -7,14 +7,18 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import team.incude.gsmc.v2.domain.evidence.application.port.ActivityEvidencePersistencePort;
+import team.incude.gsmc.v2.domain.evidence.application.port.DiscordPort;
 import team.incude.gsmc.v2.domain.evidence.application.port.OtherEvidencePersistencePort;
 import team.incude.gsmc.v2.domain.evidence.application.port.S3Port;
 import team.incude.gsmc.v2.domain.evidence.application.usecase.UpdateEvidenceFileUseCase;
 import team.incude.gsmc.v2.domain.evidence.domain.ActivityEvidence;
 import team.incude.gsmc.v2.domain.evidence.domain.OtherEvidence;
 import team.incude.gsmc.v2.domain.evidence.domain.constant.EvidenceType;
+import team.incude.gsmc.v2.global.thirdparty.aws.exception.S3UploadFailedException;
 
 import java.io.InputStream;
+import java.util.concurrent.CompletionException;
+import java.util.concurrent.ExecutionException;
 
 @Service
 @RequiredArgsConstructor
@@ -26,42 +30,52 @@ public class UpdateEvidenceFileService implements UpdateEvidenceFileUseCase {
     private final ActivityEvidencePersistencePort activityEvidencePersistencePort;
     private final OtherEvidencePersistencePort otherEvidencePersistencePort;
     private final RetryTemplate retryTemplate;
+    private final DiscordPort discordPort;
 
     @Override
-    public void execute(Long evidenceId, String fileName, InputStream inputStream, EvidenceType evidenceType) {
-        String fileUrl = uploadFile(fileName, inputStream);
+    public void execute(Long evidenceId, String fileName, InputStream inputStream, EvidenceType evidenceType, String email) {
+        String fileUrl;
+
+        fileUrl = retryTemplate.execute(context -> {
+            try {
+                return s3Port.uploadFile(fileName, inputStream).get();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                discordPort.sendEvidenceUploadFailureAlert(
+                        evidenceId,
+                        fileName,
+                        email,
+                        e
+                );
+                throw new S3UploadFailedException();
+            } catch (ExecutionException e) {
+                discordPort.sendEvidenceUploadFailureAlert(
+                        evidenceId,
+                        fileName,
+                        email,
+                        e
+                );
+                throw new S3UploadFailedException();
+            }
+        });
 
         switch (evidenceType) {
             case MAJOR, HUMANITIES -> {
                 ActivityEvidence ae = activityEvidencePersistencePort.findActivityEvidenceById(evidenceId);
-
-                activityEvidencePersistencePort.saveActivityEvidence(ae.getId(), updateActivityEvidence(ae, fileUrl));
+                activityEvidencePersistencePort.saveActivityEvidence(ae.getId(), ActivityEvidence.builder()
+                        .id(ae.getId())
+                        .title(ae.getTitle())
+                        .imageUrl(fileUrl)
+                        .content(ae.getContent())
+                        .build());
             }
             default -> {
                 OtherEvidence oe = otherEvidencePersistencePort.findOtherEvidenceById(evidenceId);
-
-                otherEvidencePersistencePort.saveOtherEvidence(oe.getId(), updateOtherEvidence(oe, fileUrl));
+                otherEvidencePersistencePort.saveOtherEvidence(oe.getId(), OtherEvidence.builder()
+                        .id(oe.getId())
+                        .fileUri(fileUrl)
+                        .build());
             }
         }
-    }
-
-    private String uploadFile(String fileName, InputStream inputStream) {
-        return retryTemplate.execute(context -> s3Port.uploadFile(fileName, inputStream).join());
-    }
-
-    private ActivityEvidence updateActivityEvidence(ActivityEvidence activityEvidence, String fileUrl) {
-        return ActivityEvidence.builder()
-                .id(activityEvidence.getId())
-                .title(activityEvidence.getTitle())
-                .imageUrl(fileUrl)
-                .content(activityEvidence.getContent())
-                .build();
-    }
-
-    private OtherEvidence updateOtherEvidence(OtherEvidence otherEvidence, String fileUrl) {
-        return OtherEvidence.builder()
-                .id(otherEvidence.getId())
-                .fileUri(fileUrl)
-                .build();
     }
 }
