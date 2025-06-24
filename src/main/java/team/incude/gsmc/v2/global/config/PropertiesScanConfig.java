0da7cf3ce533a2314d4e2424822efce603e35c4c
황지훈 <s24072@gsm.hs.kr>
2025-06-24 23:46:07 +0900

    update: 패키지 구조 변경

--- src/main/java/team/incude/gsmc/v2/global/config/PropertiesScanConfig.java
+++ src/main/java/team/incude/gsmc/v2/global/config/PropertiesScanConfig.java
@@ -1,10 +1,10 @@
-package team.incude.gsmc.v2.global.thirdparty.aws.config;
+package team.incude.gsmc.v2.global.config;
 
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import team.incude.gsmc.v2.global.thirdparty.aws.data.AwsEnvironment;
 
-@EnableConfigurationProperties(AwsEnvironment.class)
+@EnableConfigurationProperties({AwsEnvironment.class})
 @Configuration
 public class PropertiesScanConfig {
 }
