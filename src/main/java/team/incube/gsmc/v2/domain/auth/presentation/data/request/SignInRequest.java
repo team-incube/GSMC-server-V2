e0af3b9015d3bb04a02bc43293be40218f31c955
snowykte0426 <snowykte0426@naver.com>
2025-06-25 21:12:42 +0900

    fix: 비밀번호 필드 최소 길이 제한 및 이메일 필드 길이제한 통일

--- src/main/java/team/incube/gsmc/v2/domain/auth/presentation/data/request/SignInRequest.java
+++ src/main/java/team/incube/gsmc/v2/domain/auth/presentation/data/request/SignInRequest.java
@@ -7,12 +7,12 @@ import jakarta.validation.constraints.Size;
 /**
  * 로그인 요청에 사용되는 DTO입니다.
  * <p>사용자가 입력한 이메일과 비밀번호를 기반으로 인증을 수행합니다.
- * @param email 로그인할 사용자 이메일 (이메일 형식, 최대 16자)
- * @param password 로그인할 사용자 비밀번호 (최대 30자)
+ * @param email    로그인할 사용자 이메일 (이메일 형식, 최소 5자/최대 50자)
+ * @param password 로그인할 사용자 비밀번호 (최소 8자/최대 30자)
  * @author jihoonwjj
  */
 public record SignInRequest(
-        @Email @NotBlank @Size(max=16) String email,
-        @NotBlank @Size(max=30) String password
+        @Email @NotBlank @Size(min = 5, max = 50) String email,
+        @NotBlank @Size(min = 8, max = 30) String password
 ) {
 }
