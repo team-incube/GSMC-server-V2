7fa640e56414313db8e901ae0bc4ce335f576159
황지훈 <s24072@gsm.hs.kr>
2025-06-26 17:05:57 +0900

    update: JwtEnvironment PropertiesScan 추가

--- src/main/java/team/incube/gsmc/v2/global/config/PropertiesScanConfig.java
+++ src/main/java/team/incube/gsmc/v2/global/config/PropertiesScanConfig.java
@@ -2,9 +2,10 @@ package team.incube.gsmc.v2.global.config;
 
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import team.incube.gsmc.v2.global.security.jwt.data.JwtEnvironment;
 import team.incube.gsmc.v2.global.thirdparty.aws.data.AwsEnvironment;
 
-@EnableConfigurationProperties({AwsEnvironment.class})
+@EnableConfigurationProperties({AwsEnvironment.class, JwtEnvironment.class})
 @Configuration
 public class PropertiesScanConfig {
 }
