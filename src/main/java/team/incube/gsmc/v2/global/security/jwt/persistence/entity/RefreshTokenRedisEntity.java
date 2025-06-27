921ad15b6401291145eb9f276d0ee7de0aa97eb2
snowykte0426 <snowykte0426@naver.com>
2025-06-27 12:22:35 +0900

    docs: JWT 리프레시 토큰 저장 관련 클래스에 JavaDoc 주석추가

--- src/main/java/team/incube/gsmc/v2/global/security/jwt/persistence/entity/RefreshTokenRedisEntity.java
+++ src/main/java/team/incube/gsmc/v2/global/security/jwt/persistence/entity/RefreshTokenRedisEntity.java
@@ -10,6 +10,13 @@ import org.springframework.data.redis.core.index.Indexed;
 
 import java.util.concurrent.TimeUnit;
 
+/**
+ * Redis에 저장되는 Refresh Token 엔티티 클래스입니다.
+ * <p>이 클래스는 Redis에 저장될 Refresh Token의 구조를 정의하며, 토큰, 이메일, 만료 시간을 포함합니다.
+ * <p>토큰은 고유 식별자로 사용되며, 이메일은 해당 토큰과 연관된 사용자 식별을 위해 사용됩니다.
+ * 만료 시간은 토큰의 유효 기간을 나타내며, Redis에서 자동으로 삭제되도록 설정됩니다.
+ * @author jihoonwjj
+ */
 @RedisHash("refresh_token")
 @Getter
 @NoArgsConstructor
