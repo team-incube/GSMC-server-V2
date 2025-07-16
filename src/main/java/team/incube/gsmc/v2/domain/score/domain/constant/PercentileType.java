488a82828ff4bf90c6e62de8f93a82691eff46b2
snowykte0426 <snowykte0426@naver.com>
2025-07-16 23:39:23 +0900

    docs: Enum클래스 누락 JavaDoc 추가

--- src/main/java/team/incube/gsmc/v2/domain/score/domain/constant/PercentileType.java
+++ src/main/java/team/incube/gsmc/v2/domain/score/domain/constant/PercentileType.java
@@ -1,5 +1,11 @@
 package team.incube.gsmc.v2.domain.score.domain.constant;
 
+/**
+ * 성적 백분위 유형을 나타내는 열거형입니다.
+ * - HIGH: 상위 백분위수
+ * - LOW: 하위 백분위수
+ * @author jihoonwjj, snowykte0426
+ */
 public enum PercentileType {
     HIGH, LOW
 }
