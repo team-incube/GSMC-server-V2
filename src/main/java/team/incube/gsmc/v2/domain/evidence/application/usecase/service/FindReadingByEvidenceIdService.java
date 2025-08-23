7433b58cd04302593f8c29e888f83110e9bb7d69
C0ng_yun <s25069@gsm.hs.kr>
2025-08-23 22:37:31 +0900

    add: 특정 사용자 학생 기타 독서 증빙자료 조회 서비스 구현

---
+++ src/main/java/team/incube/gsmc/v2/domain/evidence/application/usecase/service/FindReadingByEvidenceIdService.java
@@ -0,0 +1,37 @@
+package team.incube.gsmc.v2.domain.evidence.application.usecase.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import team.incube.gsmc.v2.domain.evidence.application.port.ReadingEvidencePersistencePort;
+import team.incube.gsmc.v2.domain.evidence.application.usecase.FindReadingByEvidenceIdUseCase;
+import team.incube.gsmc.v2.domain.evidence.domain.ReadingEvidence;
+import team.incube.gsmc.v2.domain.evidence.exception.ActivityEvidenceAccessDeniedException;
+import team.incube.gsmc.v2.domain.evidence.presentation.data.response.GetReadingEvidenceResponse;
+import team.incube.gsmc.v2.domain.member.domain.Member;
+import team.incube.gsmc.v2.domain.member.domain.constant.MemberRole;
+import team.incube.gsmc.v2.global.security.jwt.application.usecase.service.CurrentMemberProvider;
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class FindReadingByEvidenceIdService implements FindReadingByEvidenceIdUseCase {
+    private final ReadingEvidencePersistencePort readingEvidencePersistencePort;
+    private final CurrentMemberProvider currentMemberProvider;
+    @Override
+    public GetReadingEvidenceResponse execute(Long id) {
+        Member member = currentMemberProvider.getCurrentUser();
+        ReadingEvidence evidence = readingEvidencePersistencePort.findReadingEvidenceById(id);
+        if(member.getRole().equals(MemberRole.ROLE_STUDENT)
+                &&!evidence.getId().getScore().getMember().equals(member)) {
+            throw new ActivityEvidenceAccessDeniedException();
+        }
+        return new GetReadingEvidenceResponse(
+                evidence.getId().getId(),
+                evidence.getTitle(),
+                evidence.getAuthor(),
+                evidence.getPage(),
+                evidence.getContent(),
+                evidence.getId().getReviewStatus()
+        );
+    }
+}
