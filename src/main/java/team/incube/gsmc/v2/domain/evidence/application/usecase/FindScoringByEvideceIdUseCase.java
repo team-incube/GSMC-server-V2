67489c8254b16209bc551f4376c73d03efb4a984
C0ng_yun <s25069@gsm.hs.kr>
2025-08-23 22:37:00 +0900

    add: 특정 사용자 학생 기타 점수제 증빙자료  조회 UseCase 구현

---
+++ src/main/java/team/incube/gsmc/v2/domain/evidence/application/usecase/FindScoringByEvideceIdUseCase.java
@@ -0,0 +1,7 @@
+package team.incube.gsmc.v2.domain.evidence.application.usecase;
+
+import team.incube.gsmc.v2.domain.evidence.presentation.data.response.GetOtherEvidenceResponse;
+
+public interface FindScoringByEvideceIdUseCase {
+    GetOtherEvidenceResponse execute(Long Id);
+}
