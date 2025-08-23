d0def5df07ce9e005f338edd2977ec8438031a03
C0ng_yun <s25069@gsm.hs.kr>
2025-08-23 22:34:40 +0900

    add: 특정 사용자 기타 점수제 증빙자료 조회 서비스 구현

---
+++ src/main/java/team/incube/gsmc/v2/domain/evidence/application/usecase/service/FindScoringByEvideceIdService.java
@@ -0,0 +1,39 @@
+package team.incube.gsmc.v2.domain.evidence.application.usecase.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import team.incube.gsmc.v2.domain.evidence.application.port.OtherEvidencePersistencePort;
+import team.incube.gsmc.v2.domain.evidence.application.usecase.FindScoringByEvideceIdUseCase;
+import team.incube.gsmc.v2.domain.evidence.domain.Evidence;
+import team.incube.gsmc.v2.domain.evidence.domain.OtherEvidence;
+import team.incube.gsmc.v2.domain.evidence.exception.ActivityEvidenceNotFountException;
+import team.incube.gsmc.v2.domain.evidence.presentation.data.response.GetOtherEvidenceResponse;
+import team.incube.gsmc.v2.domain.member.domain.Member;
+import team.incube.gsmc.v2.domain.member.domain.constant.MemberRole;
+import team.incube.gsmc.v2.global.security.jwt.application.usecase.service.CurrentMemberProvider;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class FindScoringByEvideceIdService implements FindScoringByEvideceIdUseCase {
+    private final CurrentMemberProvider currentMemberProvider;
+    private final OtherEvidencePersistencePort otherEvidencePersistencePort;
+    @Override
+    public GetOtherEvidenceResponse execute(Long Id) {
+        Member member = currentMemberProvider.getCurrentUser();
+        OtherEvidence evidence = otherEvidencePersistencePort.findOtherEvidenceById(Id);
+        if(member.getRole().equals(MemberRole.ROLE_STUDENT)&&
+        !evidence.getId().getScore().getMember().equals(member)) {
+            throw new ActivityEvidenceNotFountException();
+        }
+
+        return new GetOtherEvidenceResponse(
+                evidence.getId().getId(),
+                evidence.getFileUri(),
+                evidence.getId().getEvidenceType(),
+                evidence.getId().getReviewStatus(),
+                evidence.getId().getScore().getCategory().getName()
+        );
+    }
+}
