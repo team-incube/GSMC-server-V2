d3b24643f1a23bf3e52294fbb97b072d45390bd5
C0ng_yun <s25069@gsm.hs.kr>
2025-08-25 20:51:34 +0900

    delete: FindHumanitiesByEvidenceIdUseCase 삭제

--- src/main/java/team/incube/gsmc/v2/domain/evidence/application/usecase/FindHumanitiesByEvidenceIdUseCase.java
+++
@@ -1,8 +0,0 @@
-package team.incube.gsmc.v2.domain.evidence.application.usecase;
-
-import team.incube.gsmc.v2.domain.evidence.presentation.data.response.GetActivityEvidenceResponse;
-
-public interface FindHumanitiesByEvidenceIdUseCase {
-        GetActivityEvidenceResponse execute(Long id);
-    }
-
