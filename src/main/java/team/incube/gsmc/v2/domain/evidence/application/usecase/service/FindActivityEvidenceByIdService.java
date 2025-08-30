5416ce5abb6f6d5d3136da7420e6d1f5efb35f5d
C0ng_yun <s25069@gsm.hs.kr>
2025-08-30 21:51:28 +0900

    update: JavaDoc 추가 , 개행 추가

--- src/main/java/team/incube/gsmc/v2/domain/evidence/application/usecase/service/FindActivityEvidenceByIdService.java
+++ src/main/java/team/incube/gsmc/v2/domain/evidence/application/usecase/service/FindActivityEvidenceByIdService.java
@@ -10,7 +10,16 @@ import team.incube.gsmc.v2.domain.evidence.presentation.data.response.GetActivit
 import team.incube.gsmc.v2.domain.member.domain.Member;
 import team.incube.gsmc.v2.domain.member.domain.constant.MemberRole;
 import team.incube.gsmc.v2.global.security.jwt.application.usecase.service.CurrentMemberProvider;
-
+/**
+ * 활동 증빙자료 단일 조회 서비스 클래스입니다.
+ * <p>
+ * 주어진 활동 증빙자료 ID를 통해 해당 증빙자료를 조회하고,
+ * 요청한 사용자가 해당 자료를 조회할 권한이 있는지 확인하는 로직을 포함합니다.
+ * <p>{@link FindActivityEvidenceByEvidenceIdUseCase}를 구현하며,
+ * 학생 권한의 경우 자신의 자료만 조회할 수 있도록 제한합니다.
+ *
+ * @author c0ng_yun
+ */
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
