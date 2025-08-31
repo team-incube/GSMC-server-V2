49a86a2c61a752f261146e1773c03b9bc2a1e0f4
C0ng_yun <s25069@gsm.hs.kr>
2025-09-01 08:43:16 +0900

    update: Java Doc 개행제거

--- src/main/java/team/incube/gsmc/v2/domain/evidence/application/usecase/service/FindScoringByEvideceIdService.java
+++ src/main/java/team/incube/gsmc/v2/domain/evidence/application/usecase/service/FindScoringByEvideceIdService.java
@@ -14,12 +14,10 @@ import team.incube.gsmc.v2.global.security.jwt.application.usecase.service.Curre
 
 /**
  * 채점 증빙자료를 단일 조회하는 서비스 클래스입니다.
- * <p>
- * 주어진 채점 증빙자료 ID를 통해 해당 증빙자료를 조회하고,
+ * <p>주어진 채점 증빙자료 ID를 통해 해당 증빙자료를 조회하고,
  * 요청한 사용자가 해당 자료를 조회할 권한이 있는지 확인하는 로직을 포함합니다.
  * <p>{@link FindScoringByEvideceIdUseCase}를 구현하며,
  * 학생 권한의 경우 자신의 자료만 조회할 수 있도록 제한합니다.
- *
  * @author c0ng_yun
  */
 @Service
